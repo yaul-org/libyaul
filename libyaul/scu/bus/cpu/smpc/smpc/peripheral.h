16adcb3ad8cfbcac2001df3de6b9f5713549af4a
Israel Jacquez <mrkotfw@gmail.com>
2012-04-27 22:25:32 -0700

    Add digital pad peripheral

--- libyaul/scu/bus/cpu/smpc/smpc/peripheral.h
+++ libyaul/scu/bus/cpu/smpc/smpc/peripheral.h
@@ -16,6 +16,30 @@
 
 TAILQ_HEAD(multi_terminal, smpc_peripheral);
 
+struct smpc_peripheral_digital {
+        bool connected;
+        /* If no children, port is 1 or 2. Otherwise, port is under multi-terminal */
+        uint8_t port_no;
+        uint8_t type;
+        uint8_t size;
+        struct {
+                unsigned int right:1;
+                unsigned int left:1;
+                unsigned int down:1;
+                unsigned int up:1;
+                unsigned int start:1;
+                unsigned int a_trg:1;
+                unsigned int c_trg:1;
+                unsigned int b_trg:1;
+
+                unsigned int r_trg:1;
+                unsigned int x_trg:1;
+                unsigned int y_trg:1;
+                unsigned int z_trg:1;
+                unsigned int l_trg:1;
+        } __attribute__ ((packed)) button;
+} __attribute__ ((packed, __may_alias__));
+
 struct smpc_peripheral_info {
         bool connected;
         /* If no children, port is 1 or 2. Otherwise, port is under multi-terminal */
@@ -23,7 +47,7 @@ struct smpc_peripheral_info {
         uint8_t type;
         uint8_t size;
         uint8_t data[MAX_PORT_DATA_SIZE]; /* Peripheral data table */
-};
+} __attribute__ ((packed));
 
 struct smpc_peripheral {
         struct smpc_peripheral_info info;
