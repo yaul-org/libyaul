3f1c46754bd6020bd81e693794545d86924ac4d2
Israel Jacquez <mrkotfw@gmail.com>
2012-05-04 00:46:15 -0700

    Add support for line scroll screen

--- libyaul/scu/bus/b/vdp2/vdp2/scrn.h
+++ libyaul/scu/bus/b/vdp2/vdp2/scrn.h
@@ -20,14 +20,6 @@ enum scrn_type {
         SCRN_RBG1
 };
 
-enum scrn_ch_color_type {
-        SCRN_CHC_16,            /* Palette Format */
-        SCRN_CHC_256,           /* Palette Format */
-        SCRN_CHC_2048,          /* Palette Format */
-        SCRN_CHC_32768,         /* RGB Format */
-        SCRN_CHC_16770000       /* RGB Format */
-};
-
 enum scrn_rp_mode_type {
         SCRN_RP_MODE_0,         /* Rotation Parameter A */
         SCRN_RP_MODE_1,         /* Rotation Parameter B */
@@ -37,10 +29,10 @@ enum scrn_rp_mode_type {
 
 struct scrn_bm_format {
         enum scrn_type ch_scrn; /* Normal background */
-        unsigned char bm_bs;    /* Bitmap Size */
-        unsigned char bm_sp;    /* Special Priority */
-        unsigned char bm_scc;   /* Supplementary Character Number */
-        unsigned char bm_spn;   /* Supplementary Palette Number */
+        unsigned char bm_bs;    /* Bitmap size */
+        unsigned char bm_sp;    /* Special priority */
+        unsigned char bm_scc;   /* Supplementary character number */
+        unsigned char bm_spn;   /* Supplementary palette number */
 };
 
 struct scrn_ch_format {
@@ -52,15 +44,42 @@ struct scrn_ch_format {
         uint8_t ch_scc;         /* Special color calculation */
         uint8_t ch_spn;         /* Supplementary palette number */
         uint32_t ch_scn;        /* Supplementary character number (lead addr.) */
-        uint8_t ch_pls;         /* Plane size */
+        uint8_t ch_pls;         /* Plane size: (1 * 1) or (2 * 1) or (2 * 2) */
         uint32_t ch_map[4];     /* Map lead addresses */
+        uint8_t ch_pri;         /* Priority [0..7] When priority is zero, scroll screen is transparent. */
+
+#define SCRN_CH_CHC_16          0 /* Palette Format */
+#define SCRN__CH_CHC_256        1 /* Palette Format */
+#define SCRN__CH_CHC_2048       2 /* Palette Format */
+#define SCRN__CH_CHC_32768      3 /* RGB Format */
+#define SCRN__CH_CHC_16770000   4 /* RGB Format */
+        uint8_t ch_chc;         /* Character color */
+};
+
+struct scrn_ls_format {
+        enum scrn_type ls_scrn; /* Normal background */
+        uint32_t ls_lsta;       /* Line scroll table (lead addr.) */
+        uint8_t ls_int;         /* Dependent on the interlace setting */
+
+#define SCRN_LS_N0SCX   0x0002
+#define SCRN_LS_N1SCX   0x0200
+#define SCRN_LS_N0SCY   0x0004
+#define SCRN_LS_N1SCY   0x0400
+        uint16_t ls_fun;        /* Enable line scroll */
+};
+
+struct scrn_vcs_format {
+        enum scrn_type vcs_scrn;        /* Normal background */
+        uint8_t vcs_bank;               /* Bank number (0:3) */
+        uint32_t vcs_ofs;               /* Offset in VRAM bank */
 };
 
 extern void vdp2_scrn_bm_format_set(enum scrn_type, struct scrn_bm_format *);
-extern void vdp2_scrn_ch_color_set(enum scrn_type, enum scrn_ch_color_type);
 extern void vdp2_scrn_ch_format_set(struct scrn_ch_format *);
-extern void vdp2_scrn_display_set(enum scrn_type);
-extern void vdp2_scrn_priority_set(enum scrn_type, unsigned char);
+extern void vdp2_scrn_ls_set(struct scrn_ls_format *);
+extern void vdp2_scrn_vcs_set(struct scrn_vcs_format *);
+
+extern void vdp2_scrn_display_set(enum scrn_type, bool);
 extern void vdp2_scrn_rp_mode_set(enum scrn_rp_mode_type);
 
 #endif /* !_SCRN_H_ */
---
+++ libyaul/scu/bus/b/vdp2/vdp2_scrn_ls_set.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2011 Israel Jacques
+ * See LICENSE for details.
+ *
+ * Israel Jacques <mrko@eecs.berkeley.edu>
+ */
+
+#include <vdp2/scrn.h>
+
+#include <assert.h>
+
+#include "vdp2_internal.h"
+
+void
+vdp2_scrn_ls_set(struct scrn_ls_format *ls)
+{
+        uint16_t lstau;
+        uint16_t lstal;
+
+        lstau = (ls->ls_lsta >> 17) & 0x7;
+        lstal = (ls->ls_lsta >> 1) & 0xFFFF;
+
+        switch (ls->ls_scrn) {
+        case SCRN_NBG0:
+                vdp2_regs.scrctl &= 0xFFF9;
+                vdp2_regs.scrctl |= ls->ls_fun;
+
+                MEM_POKE(VDP2(LSTA0U), lstau);
+                MEM_POKE(VDP2(LSTA0L), lstal);
+                break;
+        case SCRN_NBG1:
+                vdp2_regs.scrctl &= 0xF9FD;
+                vdp2_regs.scrctl |= ls->ls_fun;
+
+                MEM_POKE(VDP2(LSTA1U), lstau);
+                MEM_POKE(VDP2(LSTA1L), lstal);
+                break;
+        default:
+                assert((ls->ls_scrn == SCRN_NBG0) || (ls->ls_scrn == SCRN_NBG1),
+                    "invalid scroll screen");
+        }
+
+        /* Write to memory */
+        MEM_POKE(VDP2(SCRCTL), vdp2_regs.scrctl);
+}
