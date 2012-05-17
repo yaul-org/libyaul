39cee7117a38d0f70c6435f45f19b711dd0f40b2
Israel Jacquez <mrkotfw@gmail.com>
2012-05-17 00:49:19 -0700

    Add common directory to build examples and tests

---
+++ examples/common.mk
@@ -0,0 +1,28 @@
+ROOTDIR?= $(dir $(lastword $(MAKEFILE_LIST)))
+CC_PREFIX?= sh-elf
+
+AS= $(CC_PREFIX)-as
+CC= $(CC_PREFIX)-gcc
+LD= $(CC_PREFIX)-gcc
+NM= $(CC_PREFIX)-nm
+OB= $(CC_PREFIX)-objcopy
+OD= $(CC_PREFIX)-objdump
+
+AFLAGS= --fatal-warnings --isa=sh2 --big --reduce-memory-overheads
+CFLAGS= -ansi \
+        -W -Wall -Wextra -Werror -Wshadow -Wunused-parameter \
+        -m2 -mb -O2 \
+        -ffast-math -fomit-frame-pointer \
+        -I../../libyaul/common \
+        -I../../libyaul/monitor \
+        -I../../libyaul/scu/bus/a/cs0 \
+        -I../../libyaul/scu/bus/a/cs1 \
+        -I../../libyaul/scu/bus/a/cs2 \
+        -I../../libyaul/scu/bus/b/scsp \
+        -I../../libyaul/scu/bus/b/vdp1 \
+        -I../../libyaul/scu/bus/b/vdp2 \
+	-I../../libyaul/scu/bus/cpu \
+        -I../../libyaul/scu/bus/cpu/smpc
+LDFLAGS= -Wl,-Map,${PROJECT}.map -nostartfiles -T $(ROOTDIR)/common/ldscripts/shelf.x
+
+OBJECTS= $(ROOTDIR)/common/crt0.o $(ROOTDIR)/common/crt0-init.o
