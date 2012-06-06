bbf57ade4773694a0a4c03612f9b17e19702d669
Israel Jacquez <mrkotfw@gmail.com>
2012-06-05 20:01:31 -0700

    Change stubs for VDP1 cons driver

--- libyaul/cons/vdp1.c
+++ libyaul/cons/vdp1.c
@@ -5,12 +5,18 @@
  * Israel Jacques <mrko@eecs.berkeley.edu>
  */
 
+#include <inttypes.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
 #include "cons.h"
 
 typedef struct {
 } cons_vdp1_t;
 
-static struct cons_vdp1_t *cons_vdp1_new(void);
+static cons_vdp1_t *cons_vdp1_new(void);
+static void cons_vdp1_reset(struct cons *);
 static void cons_vdp1_write(struct cons *, int, uint8_t, uint8_t);
 
 void
@@ -19,20 +25,37 @@ cons_vdp1_init(struct cons *cons)
         cons_vdp1_t *cons_vdp1;
 
         cons_vdp1 = cons_vdp1_new();
+
         cons->driver = cons_vdp1;
+
         cons->write = cons_vdp1_write;
+        cons->reset = cons_vdp1_reset;
 
         cons_reset(cons);
 }
 
-static struct cons_vdp1_t *
+static cons_vdp1_t *
 cons_vdp1_new(void)
 {
-        static struct cons_vdp1_t cons_vdp1;
+        /* XXX Replace with TLSF */
+        static cons_vdp1_t cons_vdp1;
         return &cons_vdp1;
+}
+
+static void
+cons_vdp1_reset(struct cons *cons)
+{
+        cons_vdp1_t *cons_vdp1;
+
+        cons_vdp1 = cons->driver;
+
+        /* Reset */
 }
 
 static void
 cons_vdp1_write(struct cons *cons, int c, uint8_t fg, uint8_t bg)
 {
+        cons_vdp1_t *cons_vdp1;
+
+        cons_vdp1 = cons->driver;
 }
