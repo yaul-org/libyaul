d4c154af0ef7926a7f879442b79bfe25fa16b93b
Israel Jacquez <mrkotfw@gmail.com>
2012-06-10 16:22:10 -0700

    Add kludge to output stack backtrace onto cons (console)

--- libyaul/common/assert.c
+++ libyaul/common/assert.c
@@ -19,38 +19,32 @@
 
 #include "stack.h"
 
-static void spin(void);
-
-void __attribute__ ((noreturn))
-_assert(const char *s, const char *cond, const char *file, uint32_t line, const char *function)
+void
+_assert(const char *s, const char *cond, const char *file, uint32_t line, const char *function,
+    const char *bt)
 {
         static char buf[16384];
+        static struct cons cons;
 
-        uint16_t blcs_color[] = {
-                0x80E0 /* Green */
+        static uint16_t blcs_color[] = {
+                0x80E0 /* Color green */
         };
 
-        struct cons cons;
-
-        (void)sprintf(buf, "%s:%lu: %s: Assertion `%s' failed: %s\n"
-            "[1B[1;44mStack backtrace[m\n%s",
-            file, line, function, cond, s, stack_backtrace());
+        (void)sprintf(buf, "%s:%lu: %s: Assertion `%s' failed: %s\n\n"
+            "Stack backtrace\n\n%s",
+            file, line, function, cond, s, bt);
 
         /* Reset the VDP2 */
         vdp2_init();
-        vdp2_tvmd_blcs_set(/* lcclmd = */ false, VRAM_ADDR_4MBIT(0, 0x00000),
+        vdp2_tvmd_blcs_set(/* lcclmd = */ false, VRAM_ADDR_4MBIT(3, 0x1FFFE),
             blcs_color, 0);
-        vdp2_tvmd_display_set(); /* Turn display ON */
 
         cons_vdp2_init(&cons);
         cons_write(&cons, buf);
 
-        spin();
-}
-
-static void __attribute__ ((noreturn))
-spin(void)
-{
+        /* XXX
+         * Refactor into using the 'abort' system-call
+         */
         while (true) {
                 vdp2_tvmd_vblank_in_wait();
                 vdp2_tvmd_vblank_out_wait();
--- libyaul/common/assert.h
+++ libyaul/common/assert.h
@@ -10,11 +10,23 @@
 
 #include <inttypes.h>
 
-void _assert(const char *, const char *, const char *, uint32_t, const char *);
+#include <stack.h>
+
+void _assert(const char *, const char *, const char *, uint32_t, const char *,
+    const char *);
 
 #define assert(c, s) do {                                                       \
                 if (!(c)) {                                                     \
-                        _assert(s, #c, __FILE__, __LINE__, __FUNCTION__);       \
+                        /*
+                         * KLUDGE
+                         *
+                         * Because GCC is too fucking retarded and just
+                         * refuses to give me a proper frame-pointer, I
+                         * have to start the backtrace here
+                         */                                                     \
+                        char *_bt;                                              \
+                        _bt = stack_backtrace();                                \
+                        _assert(s, #c, __FILE__, __LINE__, __FUNCTION__, _bt);  \
                 }                                                               \
         } while (0)
 
