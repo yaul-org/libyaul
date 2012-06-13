722991eec8c02a97c860d38571954908071e4e9f
Israel Jacquez <mrkotfw@gmail.com>
2012-06-12 22:21:13 -0700

    Add basic structures to 'fs/iso9660'

---
+++ libyaul/fs/iso9660/iso9660.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2012 Israel Jacques
+ * See LICENSE for details.
+ *
+ * Israel Jacques <mrko@eecs.berkeley.edu>
+ */
+
+#include "iso9660-def.h"
+
+#include "iso9660.h"
---
+++ libyaul/fs/iso9660/iso9660.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2011 Israel Jacques
+ * See LICENSE for details.
+ *
+ * Israel Jacques <mrko@eecs.berkeley.edu>
+ */
+
+#ifndef _ISO9660_H_
+#define _ISO9660_H_
+
+#endif /* _ISO9660_H_ */
