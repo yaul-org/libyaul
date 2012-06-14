1571f7f48fcb4f858cf3991de5b73f896976a27e
Israel Jacquez <mrkotfw@gmail.com>
2012-06-13 22:37:35 -0700

    Add basic ROMDISK FS structure

---
+++ libyaul/fs/romdisk/romdisk.c
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2001-2003, 2012
+ * See LICENSE for details.
+ *
+ * Dan Potter
+ * Lawrence Sebald
+ */
+
+#include "romdisk.h"
---
+++ libyaul/fs/romdisk/romdisk.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2012 Israel Jacques
+ * See LICENSE for details.
+ *
+ * Israel Jacques <mrko@eecs.berkeley.edu>
+ */
+
+#ifndef _ROMDISK_H_
+#define _ROMDISK_H_
+
+#endif /* _ROMDISK_H_ */
