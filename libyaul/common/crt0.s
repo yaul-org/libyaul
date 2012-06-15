e9897ed27cd4a7d2b7235d532752c2806884fd2c
Israel Jacquez <mrkotfw@gmail.com>
2012-06-15 10:19:16 -0700

    Make major cosmetic changes to all assembler files

--- libyaul/common/crt0.s
+++ libyaul/common/crt0.s
@@ -6,74 +6,74 @@
  * Israel Jacques <mrko@eecs.berkeley.edu>
  */
 
-.TEXT
-.ALIGN 1
+.text
+.align 1
 
-.GLOBAL _start
-.TYPE _start, @function
+.global _start
+.type _start, @function
 
 _start:
-        MOV.L .LC5,r15
-        MOV #0x00,r0
-        LDS r0,pr
-        MOV.L r14,@-r15
-        STS.L pr,@-r15
-        MOV r15,r14
-        /* Disable interrupts */
-        /* Purge and turn cache off */
-        MOV.L .LC6,r3
-        MOV #0x10,r1
-        MOV.B r1,@r3
-        /* Clear 'BSS' section */
-        MOV.L .LC1,r0
-        MOV.L .LC0,r1
-        MOV r0,r2
-        SUB r1,r0
-        SHLR2 r0
-        SHLR2 r0
-        MOV #0x00,r1
-        CMP/GT r1,r0
-        BF .L2
-        NOP
-.L1:
-        MOV.L r1,@-r2
-        MOV.L r1,@-r2
-        MOV.L r1,@-r2
-        DT r0
-        BF/S .L1
-        MOV.L r1,@-r2
-.L2:
-        /* Initializers */
-        MOV.L .LC3,r8
-        MOV.L .LC4,r9
-        JSR @r8
-        NOP
-        /* Enable interrupts */
-        /* Purge and turn cache on */
-        MOV.L .LC6,r1
-        MOV #0x11,r0
-        MOV.B r0,@r1
-        JSR @r9
-        NOP
-        MOV r14,r15
-        LDS.L @r15+,pr
-        MOV.L @r15+,r14
-.L02:
-        /* Disable interrupts */
-        BRA .L02
-        NOP
+        mov.l .LC5, r15
+        mov #0x00, r0
+        lds r0, pr
+        mov.l r14, @-r15
+        sts.l pr, @-r15
+        mov r15, r14
+        /* disable interrupts */
+        /* purge and turn cache off */
+        mov.l .LC6, r3
+        mov #0x10, r1
+        mov.b r1, @r3
+        /* clear 'bss' section */
+        mov.l .LC1, r0
+        mov.l .LC0, r1
+        mov r0, r2
+        sub r1, r0
+        shlr2 r0
+        shlr2 r0
+        mov #0x00, r1
+        cmp/gt r1, r0
+        bf .l2
+        nop
+.l1:
+        mov.l r1, @-r2
+        mov.l r1, @-r2
+        mov.l r1, @-r2
+        dt r0
+        bf/s .l1
+        mov.l r1, @-r2
+.l2:
+        /* initializers */
+        mov.l .LC3, r8
+        mov.l .LC4, r9
+        jsr @r8
+        nop
+        /* enable interrupts */
+        /* purge and turn cache on */
+        mov.l .LC6, r1
+        mov #0x11, r0
+        mov.b r0, @r1
+        jsr @r9
+        nop
+        mov r14, r15
+        lds.l @r15+, pr
+        mov.l @r15+, r14
+.l02:
+        /* disable interrupts */
+        bra .l02
+        nop
 
-.ALIGN 2
+.align 2
 
 .LC0:
-        .LONG __bss_start
+        .long __bss_start
 .LC1:
-        .LONG __bss_end
+        .long __bss_end
 .LC3:
-        .LONG __INIT_SECTION__
+        .long __init_section__
 .LC4:
-        .LONG _main
+        .long _main
 .LC5:
-        .LONG __stack
+        .long __stack
 .LC6:
-        .LONG 0xFFFFFE92
+        .long 0xFFFFFE92
