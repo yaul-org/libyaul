829f991ddcbb4afcb49341fa701c7f4d9a4f0198
Israel Jacquez <mrkotfw@gmail.com>
2012-06-15 12:53:57 -0700

    Make use of ARP user callback

--- examples/arp-comm/arp-comm.c
+++ examples/arp-comm/arp-comm.c
@@ -9,11 +9,37 @@
 #include <smpc.h>
 #include <smpc/peripheral.h>
 #include <arp.h>
+#include <cpu/intc.h>
 
 #include <cons/vdp2.h>
 
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+
+static struct cons cons;
+
+static void
+local_arp_cb(arp_callback_t *arp_cb)
+{
+        char *buf;
+
+        buf = (char *)malloc(1024);
+        assert(buf != NULL);
+
+        (void)sprintf(buf, "Callback\n"
+            "ptr: %p\n"
+            "len: 0x%04X\n"
+            "function: 0x%02X\n\n",
+            arp_cb->ptr,
+            arp_cb->len,
+            arp_cb->function);
+
+        cons_write(&cons, buf);
+
+        free(buf);
+}
 
 int
 main(void)
@@ -21,8 +47,6 @@ main(void)
         uint16_t blcs_color[] = {
                 0x9C00
         };
-
-        struct cons cons;
 
         vdp2_init();
         vdp2_tvmd_blcs_set(/* lcclmd = */ false, VRAM_ADDR_4MBIT(3, 0x1FFFE),
@@ -33,11 +57,16 @@ main(void)
         cons_vdp2_init(&cons);
         cons_write(&cons, "\n[1;44m     *** ARP Communication test ***     [m\n\n");
 
+        /* Register callback */
+        arp_function_callback(&local_arp_cb);
+
+        cons_write(&cons, "Ready...\n\n");
+
         while (true) {
                 vdp2_tvmd_vblank_in_wait();
                 vdp2_tvmd_vblank_out_wait();
 
-                arp_function();
+                arp_function_nonblock();
         }
 
         return 0;
