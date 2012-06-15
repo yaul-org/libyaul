b9ca166d8507c497e71ce6669acd19e77d4a2bb1
Israel Jacquez <mrkotfw@gmail.com>
2012-06-15 10:42:12 -0700

    Abort upon the user main function exiting

--- libyaul/common/crt0.s
+++ libyaul/common/crt0.s
@@ -13,18 +13,24 @@
 .type _start, @function
 
 _start:
+        /* Set stack pointer for master CPU */
         mov.l .LC5, r15
         mov #0x00, r0
         lds r0, pr
         mov.l r14, @-r15
         sts.l pr, @-r15
         mov r15, r14
-        /* disable interrupts */
-        /* purge and turn cache off */
+
+        /* Disable interrupts */
+        mov #0xF0, r1
+        ldc r1, sr
+
+        /* Purge and turn cache off */
         mov.l .LC6, r3
         mov #0x10, r1
         mov.b r1, @r3
-        /* clear 'bss' section */
+
+        /* Clear 'BSS' section */
         mov.l .LC1, r0
         mov.l .LC0, r1
         mov r0, r2
@@ -33,34 +39,48 @@ _start:
         shlr2 r0
         mov #0x00, r1
         cmp/gt r1, r0
-        bf .l2
+        bf .L2
         nop
-.l1:
+.L1:
+        mov.l r1, @-r2
         mov.l r1, @-r2
         mov.l r1, @-r2
         mov.l r1, @-r2
         dt r0
-        bf/s .l1
-        mov.l r1, @-r2
-.l2:
-        /* initializers */
-        mov.l .LC3, r8
-        mov.l .LC4, r9
-        jsr @r8
+        bf/s .L1
         nop
-        /* enable interrupts */
-        /* purge and turn cache on */
+.L2:
+        /* Initializer */
+        mov.l .LC3, r1
+        jsr @r1
+        nop
+
+        /* Purge and turn cache on */
         mov.l .LC6, r1
-        mov #0x11, r0
-        mov.b r0, @r1
-        jsr @r9
+        mov #0x11, r2
+        mov.b r2, @r1
+
+        /* Enable interrupts */
+        mov #0xFF, r2
+        mov #0x0F, r1
+        shll8 r2
+        stc sr, r3
+        or r2, r1
+        and r1, r3
+        ldc r3, sr
+
+        /* Jump to user */
+        mov.l .LC4, r3
+        jsr @r3
         nop
+
         mov r14, r15
         lds.l @r15+, pr
         mov.l @r15+, r14
-.l02:
-        /* disable interrupts */
-        bra .l02
+.L02:
+        /* Abort */
+        mov.l .LC4, r1
+        jmp @r1
         nop
 
 .align 2
@@ -70,10 +90,12 @@ _start:
 .LC1:
         .long __bss_end
 .LC3:
-        .long __init_section__
+        .long __INIT_SECTION__
 .LC4:
         .long _main
 .LC5:
         .long __stack
 .LC6:
         .long 0xFFFFFE92
+.LC7:
+        .long _abort
