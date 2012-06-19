710655b57634889518e3951cf45be0d0abd15e96
Israel Jacquez <mrkotfw@gmail.com>
2012-06-18 21:25:16 -0700

    Fix issue with not clearing all of the BSS section

--- libyaul/common/crt0.s
+++ libyaul/common/crt0.s
@@ -14,48 +14,36 @@
 
 _start:
         /* Set stack pointer for master CPU */
-        mov.l .LC5, r15
-        mov #0x00, r1
-        lds r1, pr
-        mov.l r14, @-r15
-        sts.l pr, @-r15
-        mov r15, r14
+        mov.l .LC7, r15
 
         /* Disable interrupts */
         mov #0xF0, r1
+        extu.b r1, r1
+        stc sr, r2
+        or r2, r1
         ldc r1, sr
 
-        /* Purge and turn cache off */
-        mov.l .LC6, r3
-        mov #0x10, r1
-        mov.b r1, @r3
-
         /* Clear 'BSS' section */
-        mov.l .LC1, r3
-        mov.l .LC0, r1
-        mov r3, r2
-        sub r1, r3
-        shlr2 r3
-        shlr2 r3
         mov #0x00, r1
-        cmp/gt r1, r3
-        bf .L2
-        nop
+        mov.l .LC0, r2
+        mov.l .LC1, r3
+        cmp/eq r2, r3
+        bt .L2
 .L1:
-        mov.l r1, @-r2
-        mov.l r1, @-r2
-        mov.l r1, @-r2
-        mov.l r1, @-r2
-        dt r3
-        bf/s .L1
-        nop
+        mov.l r1, @r3
+        mov.l r1, @-r3
+        mov.l r1, @-r3
+        mov.l r1, @-r3
+        mov.l r1, @-r3
+        cmp/eq r2, r3
+        bf .L1
 .L2:
         /* Initializer */
         mov.l .LC3, r1
         jsr @r1
         nop
 
-        /* Purge and turn cache on */
+        /* Purge and turn cache on as four-way set associative */
         mov.l .LC6, r1
         mov #0x11, r2
         mov.b r2, @r1
@@ -74,28 +62,30 @@ _start:
         jsr @r3
         nop
 
-        mov r14, r15
-        lds.l @r15+, pr
-        mov.l @r15+, r14
-.L02:
         /* Abort */
-        mov.l .LC4, r1
+        mov.l .LC5, r1
         jmp @r1
         nop
 
 .align 2
 
 .LC0:
-        .long __bss_start
+.long __bss_start
+
 .LC1:
-        .long __bss_end
+.long __bss_end
+
 .LC3:
-        .long __INIT_SECTION__
+.long __INIT_SECTION__
+
 .LC4:
-        .long _main
+.long _main
+
 .LC5:
-        .long __stack
+.long _abort
+
 .LC6:
-        .long 0xFFFFFE92
+.long 0xFFFFFE92
+
 .LC7:
-        .long _abort
+.long __stack
