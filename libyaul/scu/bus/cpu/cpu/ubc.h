0f0944f502b1679d44e6ab77b31362ead45674dd
Israel Jacquez <mrkotfw@gmail.com>
2012-06-24 00:40:09 -0700

    Add stubs for UBC

---
+++ libyaul/scu/bus/cpu/cpu/ubc.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2012 Israel Jacques
+ * See LICENSE for details.
+ *
+ * Israel Jacques <mrko@eecs.berkeley.edu>
+ */
+
+#ifndef _UBC_H_
+#define _UBC_H_
+
+#endif /* !_UBC_H */
---
+++ libyaul/scu/bus/cpu/cpu_ubc_init.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2012 Israel Jacques
+ * See LICENSE for details.
+ *
+ * Israel Jacques <mrko@eecs.berkeley.edu>
+ */
+
+#include <cpu/dmac.h>
+#include <cpu/intc.h>
+
+#include "cpu-internal.h"
+
+void
+cpu_ubc_init(void)
+{
+}
