395381e4803517e26597965e94def4976aaf531b
Israel Jacquez <mrkotfw@gmail.com>
2013-01-31 18:07:02 -0800

    Add C++ support for examples

--- examples/common.mk
+++ examples/common.mk
@@ -4,13 +4,14 @@ CC_PREFIX?= sh-elf
 AS= $(CC_PREFIX)-as
 CC= $(CC_PREFIX)-gcc
 LD= $(CC_PREFIX)-gcc
+CXX= $(CC_PREFIX)-g++
 NM= $(CC_PREFIX)-nm
 OB= $(CC_PREFIX)-objcopy
 OD= $(CC_PREFIX)-objdump
 
 AFLAGS= --fatal-warnings --isa=sh2 --big --reduce-memory-overheads \
 	-I$(ROOTDIR)/common
-CFLAGS= -W -Wall -Wextra -Werror -Wshadow -Wunused-parameter -Wstrict-aliasing \
+CFLAGS= -W -Wall -Wextra -Werror -Wunused-parameter -Wstrict-aliasing \
 	-ansi -m2 -mb -O2 -fomit-frame-pointer \
 	-ffast-math -fstrict-aliasing \
 	-I../../libyaul/common \
@@ -32,6 +33,14 @@ LDFLAGS= -Wl,-Map,${PROJECT}.map \
 	-Wl,--end-group \
 	-Wl,--gc-sections \
 	-nostartfiles -T $(ROOTDIR)/common/ldscripts/sh-elf.x
+CXXFLAGS= $(CFLAGS) -ffreestanding -fno-exceptions -fno-rtti
+LXXFLAGS= -Wl,-Map,${PROJECT}.map \
+	-L$(ROOTDIR)/../build/libyaul \
+	-Wl,--start-group -lyaul -lstdc++ -lc -lgcc \
+	-Wl,--end-group \
+	-Wl,--gc-sections \
+	-nostartfiles -T $(ROOTDIR)/common/ldscripts/sh-elf.x \
+	-ffreestanding -fno-exceptions -fno-rtti
 
 # All programs must link this as the first object (crt0.o)
 OBJECTS:= $(ROOTDIR)/common/crt0.o \
