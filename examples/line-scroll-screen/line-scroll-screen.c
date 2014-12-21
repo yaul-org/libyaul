a6a351804be5fb3f434a8f7770b80c92b4594ecb
Israel Jacquez <mrkotfw@gmail.com>
2014-12-21 14:37:47 -0800

    Fix compilation errors for example line-scroll-screen
    This example still isn't working completely yet. The actual update
    to the line scroll table doesn't seem to work very well.

--- examples/line-scroll-screen/line-scroll-screen.c
+++ examples/line-scroll-screen/line-scroll-screen.c
@@ -12,7 +12,7 @@
 
 #include "lut_sin.h"
 
-#define RGB(r, g, b) (0x8000 | ((r) & 0x001F) | (((g) & 0x001F)  << 5) | (((b) & 0x001F) << 10))
+#define RGB888_TO_RGB555(r, g, b) ((((b) >> 3) << 10) | (((g) >> 3) << 5) | ((r) >> 3))
 
 static uint32_t *line_scroll_tb = (uint32_t *)VRAM_ADDR_4MBIT(2, 0x1F000);
 
@@ -32,41 +32,37 @@ line_scroll(void)
         ofs += 4;
 }
 
+static uint16_t *_nbg1_planes[4] = {
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x1C000),
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x1C000),
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x1C000),
+        /* VRAM B0 */
+        (uint16_t *)VRAM_ADDR_4MBIT(2, 0x1C000)
+};
+static uint32_t *_nbg1_character = (uint32_t *)VRAM_ADDR_4MBIT(2, 0x1EFC0);
+
 static void
 line_scroll_screen(void)
 {
-        struct scrn_ch_format cfg;
+        struct scrn_cell_format nbg1_format;
         struct vram_ctl *vram_ctl;
 
-        uint16_t *cram;
-        uint16_t *pnt;
-        uint32_t *character;
-        uint16_t pntv;
-        uint32_t x;
-        uint32_t y;
-        uint32_t character_ofs;
+        nbg1_format.scf_scroll_screen = SCRN_NBG1;
+        nbg1_format.scf_cc_count = SCRN_CCC_PALETTE_16;
+        nbg1_format.scf_character_size= 1 * 1;
+        nbg1_format.scf_pnd_size = 1; /* 1 word */
+        nbg1_format.scf_auxiliary_mode = 1;
+        nbg1_format.scf_cp_table = (uint32_t)_nbg1_character;
+        nbg1_format.scf_plane_size = 1 * 1;
+        nbg1_format.scf_map.plane_a = (uint32_t)_nbg1_planes[0];
+        nbg1_format.scf_map.plane_b = (uint32_t)_nbg1_planes[1];
+        nbg1_format.scf_map.plane_c = (uint32_t)_nbg1_planes[2];
+        nbg1_format.scf_map.plane_d = (uint32_t)_nbg1_planes[3];
 
-        character_ofs = 0x1EFC0;
-        character = (uint32_t *)VRAM_ADDR_4MBIT(2, character_ofs);
-        pnt = (uint16_t *)VRAM_ADDR_4MBIT(2, 0x1C000);
-        cram = (uint16_t *)CRAM_BANK(0, 0);
-
-        cfg.ch_scrn = SCRN_NBG1;
-        cfg.ch_cs = 1 * 1; /* 1x1 cells */
-        cfg.ch_pnds = 1; /* 1 word */
-        cfg.ch_cnsm = 1; /* Character number supplement mode: 1 */
-        cfg.ch_sp = 0;
-        cfg.ch_scc = 0;
-        cfg.ch_spn = 0;
-        cfg.ch_scn = (uint32_t)character;
-        cfg.ch_pls = 1 * 1; /* 1x1 plane size */
-        cfg.ch_map[0] = (uint32_t)pnt;
-        cfg.ch_map[1] = (uint32_t)pnt;
-        cfg.ch_map[2] = (uint32_t)pnt;
-        cfg.ch_map[3] = (uint32_t)pnt;
-
-        vdp2_scrn_ccc_set(SCRN_NBG1, SCRN_CCC_CHC_16);
-        vdp2_scrn_ch_format_set(&cfg);
+        vdp2_scrn_cell_format_set(&nbg1_format);
         vdp2_priority_spn_set(SCRN_NBG1, 7);
 
         vram_ctl = vdp2_vram_control_get();
@@ -80,32 +76,57 @@ line_scroll_screen(void)
         vram_ctl->vram_cycp.pt[2].t0 = VRAM_CTL_CYCP_NO_ACCESS;
         vdp2_vram_control_set(vram_ctl);
 
-        /* Black color #0 */
-        *cram++ = RGB(0, 0, 0);
-        /* White color #1 */
-        *cram = RGB(31, 31, 31);
+        /* Set palette */
+        {
+                /* Black color #0 */
+                uint16_t *cram;
+                cram = (uint16_t *)CRAM_BANK(0, 0);
 
-        /* Draw two tiles */
-        y = 0;
-        *character++ = 0x11111111;
-        for (y = 1; y < 7; y++)
-                *character++ = 0x10000001;
-        *character++ = 0x11111111;
-        for (y = 0; y < 8; y++)
-                *character++ = 0x00000000;
-        character = (uint32_t *)VRAM_ADDR_4MBIT(2, character_ofs);
+                *cram++ = RGB888_TO_RGB555(0, 0, 0);
+                /* White color #1 */
+                *cram = RGB888_TO_RGB555(255, 255, 255);
+        }
 
-        /* Set up the map */
-        for (y = 0; y < 64; y++) {
-                for (x = 0; x < 64; x++) {
-                        pntv = PN_CHARACTER_NO((uint32_t)character);
-                        pnt[x + (y << 6)] = pntv | ((x + y) % 2);
+        /* Draw cells */
+        {
+                uint32_t *vram;
+                vram = _nbg1_character;
+
+                uint32_t i;
+
+                *vram++ = 0x11111111;
+                for (i = 1; i < 7; i++) {
+                        *vram++ = 0x10000001;
+                }
+                *vram++ = 0x11111111;
+                for (i = 0; i < 8; i++) {
+                        *vram++ = 0x00000000;
                 }
         }
 
-        for (y = 0; y < 512; y++) {
-                line_scroll_tb[y] = (lut_sin[y]) << 16;
-                line_scroll_tb[y + 512] = (lut_sin[y]) << 16;
+        /* Set up the map */
+        {
+                uint16_t *pnt;
+                pnt = (uint16_t *)VRAM_ADDR_4MBIT(2, 0x1C000);
+
+                uint32_t x;
+                uint32_t y;
+                for (y = 0; y < 64; y++) {
+                        for (x = 0; x < 64; x++) {
+                                uint16_t pntv;
+                                pntv = VDP2_PN_CONFIG_1_CHARACTER_NUMBER((uint32_t)_nbg1_character);
+                                pnt[x + (y << 6)] = pntv | ((x + y) % 2);
+                        }
+                }
+        }
+
+        /* Set up the line scroll table */
+        {
+                uint32_t y;
+                for (y = 0; y < 512; y++) {
+                        line_scroll_tb[y] = (lut_sin[y]) << 16;
+                        line_scroll_tb[y + 512] = (lut_sin[y]) << 16;
+                }
         }
 
         vdp2_scrn_display_set(SCRN_NBG1, /* no_trans = */ false);
@@ -115,12 +136,12 @@ int
 main(void)
 {
         uint16_t blcs_color[] = {
-                RGB(0, 15, 15)
+                RGB888_TO_RGB555(255, 0, 0)
         };
 
         vdp2_init();
-        vdp2_tvmd_blcs_set(/* lcclmd = */ false, VRAM_ADDR_4MBIT(3, 0x1FFFE),
-            blcs_color, 0);
+        vdp2_scrn_back_screen_set(/* single_color = */ true,
+            VRAM_ADDR_4MBIT(3, 0x1FFFE), blcs_color, 0);
 
         line_scroll_screen();
 
