3e50eb1bfdab35b28923f552e484873792b41d2a
Israel Jacquez <mrkotfw@gmail.com>
2014-12-21 00:52:43 -0800

    Fix compilation errors for example romdisk

--- examples/romdisk/romdisk.c
+++ examples/romdisk/romdisk.c
@@ -9,7 +9,7 @@
 #include <smpc.h>
 #include <smpc/peripheral.h>
 
-#include <cons/vdp2.h>
+#include <cons.h>
 #include <fs/romdisk/romdisk.h>
 
 #include <assert.h>
@@ -20,6 +20,7 @@
 extern uint8_t root_romdisk[];
 
 static void delay(uint16_t);
+static void sync(void);
 
 int
 main(void)
@@ -37,35 +38,49 @@ main(void)
         void *fh; /* File handle */
 
         vdp2_init();
-        vdp2_tvmd_blcs_set(/* lcclmd = */ false, VRAM_ADDR_4MBIT(3, 0x1FFFE),
+        vdp2_scrn_back_screen_set(/* single_color = */ true, VRAM_ADDR_4MBIT(2, 0x1FFFE),
             blcs_color, 0);
 
         smpc_init();
 
-        cons_vdp2_init(&cons);
-        cons_write(&cons, "\n[1;44m          *** ROMDISK Test ***          [m\n\n");
+        cons_init(&cons, CONS_DRIVER_VDP2);
 
-        cons_write(&cons, "Mounting ROMDISK... ");
+        cons_buffer(&cons, "\n[1;44m          *** ROMDISK Test ***          [m\n\n");
+
+        cons_buffer(&cons, "Mounting ROMDISK... ");
+
         romdisk_init();
 
         romdisk = romdisk_mount("/", root_romdisk);
 
-        delay(1);
-
-        if (romdisk != NULL)
-                cons_write(&cons, "OK!\n");
+        sync();
+        cons_write(&cons);
 
         delay(1);
 
-        cons_write(&cons, "Opening \"/tmp/txt/hello.world\" ");
+        if (romdisk != NULL) {
+                cons_buffer(&cons, "OK!\n");
+                sync();
+                cons_write(&cons);
+        }
+
+        delay(1);
+
+        cons_buffer(&cons, "Opening \"/tmp/txt/hello.world\"... ");
+        sync();
+        cons_write(&cons);
         if ((fh = romdisk_open(romdisk, "/tmp/txt/hello.world", O_RDONLY)) == NULL) {
-                cons_write(&cons, "[1;31mFAILED[0m\n");
+                cons_buffer(&cons, "[1;31mFAILED[0m\n");
+                sync();
+                cons_write(&cons);
                 abort();
         }
 
         delay(1);
 
-        cons_write(&cons, "OK!\n");
+        cons_buffer(&cons, "OK!\n");
+        sync();
+        cons_write(&cons);
 
         msg_len = romdisk_total(fh);
         msg = (char *)malloc(msg_len + 1);
@@ -74,21 +89,31 @@ main(void)
 
         delay(1);
 
-        cons_write(&cons, "Reading... ");
+        cons_buffer(&cons, "Reading... ");
+        sync();
+        cons_write(&cons);
         msg_len = romdisk_read(fh, msg, romdisk_total(fh));
 
         delay(1);
 
+        cons_buffer(&cons, "OK!\n");
+        sync();
+        cons_write(&cons);
+
         if (msg_len == 0) {
-                cons_write(&cons, "FAILED\n");
+                cons_buffer(&cons, "FAILED\n");
+                sync();
+                cons_write(&cons);
                 abort();
         }
 
-        cons_write(&cons, "\n[1;33m");
-        cons_write(&cons, msg);
-        cons_write(&cons, "[m");
+        cons_buffer(&cons, "\n[1;33m");
+        cons_buffer(&cons, msg);
+        cons_buffer(&cons, "[m");
 
-        cons_write(&cons, "\nTest complete!\n");
+        cons_buffer(&cons, "\nTest complete!\n");
+        sync();
+        cons_write(&cons);
 
         abort();
 }
@@ -99,7 +124,13 @@ delay(uint16_t t)
         uint16_t frame;
 
         for (frame = 0; frame < (60 * t); frame++) {
-                vdp2_tvmd_vblank_in_wait();
-                vdp2_tvmd_vblank_out_wait();
+                sync();
         }
+}
+
+static void
+sync(void)
+{
+        vdp2_tvmd_vblank_out_wait();
+        vdp2_tvmd_vblank_in_wait();
 }
