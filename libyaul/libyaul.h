748671ca77b952e525ea3085490a272a5198a416
Israel Jacquez <mrkotfw@gmail.com>
2014-12-21 22:02:30 -0800

    Add main header
    This can be used to simply include all the usable headers in a
    project.

---
+++ libyaul/libyaul.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2012 Israel Jacquez
+ * See LICENSE for details.
+ *
+ * Israel Jacquez <mrkotfw@gmail.com>
+ */
+
+#ifndef _LIBYAUL_H_
+#define _LIBYAUL_H_
+
+/* CPU */
+#include <cpu.h>
+
+/* SCU */
+#include <scu.h>
+
+/* CS0 */
+#include <arp.h>
+#include <dram-cartridge.h>
+#include <usb-cartridge.h>
+
+/* CS2 */
+#include <cd-block.h>
+
+/* VDP1 */
+#include <vdp1.h>
+
+/* VDP2 */
+#include <vdp2.h>
+
+/* SMPC */
+#include <smpc.h>
+
+/* Kernel */
+#include <common.h>
+#include <irq-mux.h>
+#include <cons.h>
+
+#include <fs/iso9660/iso9660.h>
+#include <fs/romdisk/romdisk.h>
+
+#endif /* !_LIBYAUL_H_ */
