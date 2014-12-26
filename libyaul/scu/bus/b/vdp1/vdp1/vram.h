954eb21869e9c4ed8d6a63cb44a2b195f32d2b98
Israel Jacquez <mrkotfw@gmail.com>
2014-12-25 22:28:24 -0800

    Fix error in VDP1 VRAM character offset

--- libyaul/scu/bus/b/vdp1/vdp1/vram.h
+++ libyaul/scu/bus/b/vdp1/vdp1/vram.h
@@ -16,20 +16,26 @@ extern "C" {
 #endif /* __cplusplus */
 
 #define VDP1_CMDT_COUNT_MAX     544
-#define VDP1_CMDT_MEMORY_SIZE   (VDP1_CMDT_COUNT_MAX * sizeof(struct vdp1_cmdt)) /* In bytes */
+#define VDP1_CMDT_MEMORY_SIZE   (VDP1_CMDT_COUNT_MAX *                         \
+            sizeof(struct vdp1_cmdt)) /* In bytes */
 
 #define VDP1_TEXURE_MEMORY_SIZE ((486) * (1 << 10)) /* In bytes */
 
 #define VDP1_GST_COUNT_MAX      128
-#define VDP1_GST_MEMORY_SIZE    (VDP1_GST_COUNT_MAX * sizeof(struct vdp1_cmdt_gst)) /* In bytes */
+#define VDP1_GST_MEMORY_SIZE    (VDP1_GST_COUNT_MAX *                          \
+            sizeof(struct vdp1_cmdt_gst)) /* In bytes */
 
 #define VDP1_CLUT_COUNT_MAX     256
-#define VDP1_CLUT_MEMORY_SIZE   (VDP1_CLUT_COUNT_MAX * (16 * sizeof(uint16_t))) /* In bytes */
+#define VDP1_CLUT_MEMORY_SIZE   (VDP1_CLUT_COUNT_MAX *                         \
+            (16 * sizeof(uint16_t))) /* In bytes */
 
 #define CMD_TABLE(x, y)         (0x25C00000 + ((x) << 5) + (((y) << 1) & 0x1F))
-#define CLUT(x, y)              (0x25C00000 + VDP1_CMDT_MEMORY_SIZE + ((x) << 4) + ((y) << 1))
-#define GOURAUD(x, y)           (0x25C00000 + VDP1_CMDT_MEMORY_SIZE + VDP1_CLUT_MEMORY_SIZE + ((x) << 4) + ((y) << 1))
-#define CHAR(x)                 (0x25C00000 + VDP1_CMDT_MEMORY_SIZE + VDP1_TEXURE_MEMORY_SIZE + VDP1_GST_MEMORY_SIZE + ((x)))
+#define CLUT(x, y)              (0x25C00000 + VDP1_CMDT_MEMORY_SIZE +          \
+            ((x) << 4) + ((y) << 1))
+#define GOURAUD(x, y)           (0x25C00000 + VDP1_CMDT_MEMORY_SIZE +          \
+            VDP1_CLUT_MEMORY_SIZE + ((x) << 4) + ((y) << 1))
+#define CHAR(x)                 (0x25C00000 + VDP1_CMDT_MEMORY_SIZE +          \
+            VDP1_CLUT_MEMORY_SIZE + VDP1_GST_MEMORY_SIZE + ((x)))
 #define VDP1_FRAME_BUFFER(x)    (0x25C80000 + ((x)))
 
 #ifdef __cplusplus
