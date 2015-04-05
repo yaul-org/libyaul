8171121b8468e62bb3763d10c97192b8b8deb828
Israel Jacquez <mrkotfw@gmail.com>
2015-04-04 17:00:07 -0700

    Refactor polygon sorting
    It's still not optimal.

--- examples/vdp1/tests/07.c
+++ examples/vdp1/tests/07.c
@@ -27,10 +27,10 @@
 #define PLANE_POLYGON_CNT 1
 
 static fix16_vector4_t __unused plane_vertices[] = {
-        FIX16_VERTEX4(-0.25f,  0.25f, -0.25f, 1.0f),
-        FIX16_VERTEX4( 0.25f,  0.25f, -0.25f, 1.0f),
-        FIX16_VERTEX4( 0.25f, -0.25f, -0.25f, 1.0f),
-        FIX16_VERTEX4(-0.25f, -0.25f, -0.25f, 1.0f),
+        FIX16_VERTEX4(-0.25f,  0.25f, 0.0f, 1.0f),
+        FIX16_VERTEX4( 0.25f,  0.25f, 0.0f, 1.0f),
+        FIX16_VERTEX4( 0.25f, -0.25f, 0.0f, 1.0f),
+        FIX16_VERTEX4(-0.25f, -0.25f, 0.0f, 1.0f),
 };
 
 static uint32_t __unused plane_indices[PLANE_POLYGON_CNT * 4] = {
@@ -44,15 +44,15 @@ static fix16_vector4_t __unused plane_normals[PLANE_POLYGON_CNT] = {
 #define CUBE_POLYGON_CNT 6
 
 static fix16_vector4_t __unused cube_vertices[] = {
+        FIX16_VERTEX4(-0.25f,  0.25f,  0.25f, 1.0f),
+        FIX16_VERTEX4( 0.25f,  0.25f,  0.25f, 1.0f),
+        FIX16_VERTEX4( 0.25f, -0.25f,  0.25f, 1.0f),
+        FIX16_VERTEX4(-0.25f, -0.25f,  0.25f, 1.0f),
+
         FIX16_VERTEX4(-0.25f,  0.25f, -0.25f, 1.0f),
         FIX16_VERTEX4( 0.25f,  0.25f, -0.25f, 1.0f),
         FIX16_VERTEX4( 0.25f, -0.25f, -0.25f, 1.0f),
-        FIX16_VERTEX4(-0.25f, -0.25f, -0.25f, 1.0f),
-
-        FIX16_VERTEX4(-0.25f,  0.25f, -0.75f, 1.0f),
-        FIX16_VERTEX4( 0.25f,  0.25f, -0.75f, 1.0f),
-        FIX16_VERTEX4( 0.25f, -0.25f, -0.75f, 1.0f),
-        FIX16_VERTEX4(-0.25f, -0.25f, -0.75f, 1.0f)
+        FIX16_VERTEX4(-0.25f, -0.25f, -0.25f, 1.0f)
 };
 
 /*
@@ -1690,15 +1690,12 @@ static fix16_vector4_t __unused teapot_normals[] = {
 static uint16_t colors[TEAPOT_POLYGON_CNT] __unused;
 
 
-#define OT_PRIMITIVE_BUCKETS    1024
+#define OT_PRIMITIVE_BUCKETS    1
 #define OT_PRIMITIVE_CNT        VDP1_CMDT_COUNT_MAX
 
 #define OT_PRIMITIVE_BUCKET_SORT_INSERTION      0
 #define OT_PRIMITIVE_BUCKET_SORT_BUBBLE         1
-
-struct ot_primitive;
-
-TAILQ_HEAD(ot_primitive_buckets, ot_primitive) ot_buckets;
+#define OT_PRIMITIVE_BUCKET_SORT_QUICK          2
 
 struct ot_primitive {
         uint16_t otp_color;
@@ -1708,15 +1705,22 @@ struct ot_primitive {
         TAILQ_ENTRY(ot_primitive) otp_entries;
 } __aligned(32);
 
+struct ot_primitive_bucket {
+        TAILQ_HEAD(, ot_primitive) opb_bucket;
+        uint32_t opb_count;
+};
+
 /* OT */
-static struct ot_primitive_buckets ot_primitive_buckets[OT_PRIMITIVE_BUCKETS] __unused;
+static struct ot_primitive_bucket ot_primitive_bucket_pool[OT_PRIMITIVE_BUCKETS] __unused;
 static struct ot_primitive ot_primitive_pool[OT_PRIMITIVE_CNT] __unused;
 
 static uint32_t ot_primitive_pool_idx;
 
 static void ot_init(void);
-static void ot_primitive_add(const fix16_vector4_t *, uint16_t);
-static void ot_primitive_bucket_sort(int32_t, int32_t);
+static void ot_primitive_add(const fix16_vector4_t *, const fix16_vector4_t *,
+    uint16_t);
+static void ot_bucket_init(int32_t);
+static void ot_bucket_primitive_sort(int32_t, int32_t);
 
 
 static void model_polygon_project(const fix16_vector4_t *, const uint32_t *,
@@ -1727,6 +1731,7 @@ static void model_polygon_project(const fix16_vector4_t *, const uint32_t *,
 #define MODEL_PROJECT           1 /* 0: Upload          1: Upload and project */
 #define POLYGON_SORT            1 /* 0: No sort         1: Sort */
 #define RENDER                  1 /* 0: No render       1: Render */
+#define CULLING                 1 /* 0: No culling      1: Culling */
 
 
 static struct vdp1_cmdt_polygon __unused polygon;
@@ -1735,13 +1740,6 @@ void
 test_07_init(void)
 {
         init();
-
-        colors[0] = RGB888_TO_RGB555(255,   0,   0);
-        colors[1] = RGB888_TO_RGB555(  0, 255,   0);
-        colors[2] = RGB888_TO_RGB555(  0,   0, 255);
-        colors[3] = RGB888_TO_RGB555(255, 255,   0);
-        colors[4] = RGB888_TO_RGB555(255,   0, 255);
-        colors[5] = RGB888_TO_RGB555(255, 255, 255);
 
         uint32_t color_idx;
         for (color_idx = 0; color_idx < TEAPOT_POLYGON_CNT; color_idx++) {
@@ -1773,6 +1771,13 @@ test_07_init(void)
                 colors[color_idx] = RGB888_TO_RGB555(color, color, color);
         }
 
+        colors[0] = RGB888_TO_RGB555(255,   0,   0);
+        colors[1] = RGB888_TO_RGB555(  0, 255,   0);
+        colors[2] = RGB888_TO_RGB555(  0,   0, 255);
+        colors[3] = RGB888_TO_RGB555(255, 255,   0);
+        colors[4] = RGB888_TO_RGB555(255,   0, 255);
+        colors[5] = RGB888_TO_RGB555(255, 255, 255);
+
         ot_init();
         matrix_stack_init();
 
@@ -1780,9 +1785,12 @@ test_07_init(void)
 
         fix16_t ratio;
         ratio = F16((float)SCREEN_WIDTH / (float)SCREEN_HEIGHT);
+        ratio = F16(1.4f);
 
-        matrix_stack_orthographic_project(-ratio, ratio, F16(1.0f), F16(-1.0f),
-            F16(1.0f), F16(1000.0f));
+        matrix_stack_orthographic_project(-fix16_mul(F16(100.0f), ratio),
+            fix16_mul(F16(100.0f), ratio),
+            F16(100.0f), F16(-100.0f),
+            F16(100.0f), F16(100.0f));
 
         matrix_stack_mode(MATRIX_STACK_MODE_MODEL_VIEW);
         matrix_stack_translate(F16(0.0f), F16(0.0f), F16(-10.0f));
@@ -1798,7 +1806,7 @@ test_07_init(void)
 void
 test_07_update(void)
 {
-        static fix16_t angle __unused = F16(-1.0f);
+        static fix16_t angle __unused = F16(0.0f);
 
         if (digital_pad.connected == 1) {
                 if (digital_pad.held.button.start) {
@@ -1816,23 +1824,16 @@ test_07_update(void)
 
         matrix_stack_mode(MATRIX_STACK_MODE_MODEL_VIEW);
         matrix_stack_push(); {
-                /* C=VM
-                 * D=CR=V(MR)
-                 * E=DS=V(MRS) */
-#if MODEL_TRANSFORMATIONS == 1
-                matrix_stack_translate(F16(0.0f), F16(0.0f), F16(0.0f));
-                matrix_stack_rotate(fix16_mul(F16(2.0f), angle), 0);
+                matrix_stack_scale(F16(100.0f), F16(100.0f), F16(100.0f));
+                matrix_stack_rotate(angle, 0);
                 matrix_stack_rotate(angle, 1);
                 matrix_stack_rotate(angle, 2);
-#endif
 
-#if MODEL_PROJECT == 1
                 model_polygon_project(teapot_vertices, teapot_indices,
                     teapot_normals, TEAPOT_POLYGON_CNT);
-#endif
         } matrix_stack_pop();
 
-        angle = fix16_add(angle, F16(-2.0f));
+        angle = fix16_add(angle, F16(-1.0f));
 
         int32_t buckets;
         buckets = 0;
@@ -1844,25 +1845,25 @@ test_07_update(void)
                 int32_t idx;
                 for (idx = OT_PRIMITIVE_BUCKETS - 1; idx >= 0; idx--) {
                         /* Skip empty buckets */
-                        if (TAILQ_EMPTY(&ot_primitive_buckets[idx])) {
+                        if (TAILQ_EMPTY(&ot_primitive_bucket_pool[idx].opb_bucket)) {
                                 continue;
                         }
 
                         buckets++;
 
 #if POLYGON_SORT == 1
-                        ot_primitive_bucket_sort(idx & (OT_PRIMITIVE_BUCKETS - 1),
-                                OT_PRIMITIVE_BUCKET_SORT_INSERTION);
+                        ot_bucket_primitive_sort(idx & (OT_PRIMITIVE_BUCKETS - 1),
+                            OT_PRIMITIVE_BUCKET_SORT_INSERTION);
 #endif
 #if RENDER == 1
                         struct ot_primitive *otp;
-                        TAILQ_FOREACH (otp, &ot_primitive_buckets[idx], otp_entries) {
+                        TAILQ_FOREACH (otp, &ot_primitive_bucket_pool[idx].opb_bucket, otp_entries) {
                                 polygons++;
 
                                 polygon.cp_color = otp->otp_color;
                                 polygon.cp_mode.transparent_pixel = true;
-
                                 polygon.cp_mode.end_code = true;
+
                                 polygon.cp_vertex.a.x = otp->otp_coords[0].x;
                                 polygon.cp_vertex.a.y = otp->otp_coords[0].y;
                                 polygon.cp_vertex.b.x = otp->otp_coords[1].x;
@@ -1876,7 +1877,7 @@ test_07_update(void)
                         }
 #endif
                         /* Clear OT bucket */
-                        TAILQ_INIT(&ot_primitive_buckets[idx]);
+                        ot_bucket_init(idx);
                 }
                 vdp1_cmdt_end();
         } vdp1_cmdt_list_end(1);
@@ -1890,7 +1891,7 @@ test_07_update(void)
         (void)sprintf(text, "Tick diff: %i-%i=%i\n"
             "Scanline diff: %i-%i=%i\n"
             "Buckets: %i\n"
-            "Polygons: %i/%i",
+            "Polygons: %i/%i\n",
             (int)end_tick,
             (int)start_tick,
             (int)(end_tick - start_tick),
@@ -1922,7 +1923,8 @@ static void __unused
 model_polygon_project(const fix16_vector4_t *vb, const uint32_t *ib,
     const fix16_vector4_t *nb, const uint32_t ib_cnt)
 {
-        fix16_vector4_t projected_polygon[4];
+        fix16_vector4_t vertex_mv[4];
+        fix16_vector4_t vertex_projected[4];
 
         fix16_matrix4_t *matrix_projection;
         matrix_projection = matrix_stack_top(
@@ -1933,13 +1935,10 @@ model_polygon_project(const fix16_vector4_t *vb, const uint32_t *ib,
                 MATRIX_STACK_MODE_MODEL_VIEW)->ms_matrix;
 
         /* PVMv = (P(VM))v */
-        fix16_matrix4_t matrix_mvp;
-        fix16_matrix4_multiply(matrix_projection, matrix_model_view,
-            &matrix_mvp);
 
-        /* Get inverse matrix */
-        fix16_matrix4_t matrix_mv_inv;
-        fix16_matrix4_inverse(matrix_model_view, &matrix_mv_inv);
+        /* Calculate world to object space matrix (inverse) */
+        fix16_matrix4_t matrix_wo;
+        fix16_matrix4_inverse(matrix_model_view, &matrix_wo);
 
         fix16_vector4_t view_forward;
         view_forward.x = F16(0.0f);
@@ -1947,9 +1946,9 @@ model_polygon_project(const fix16_vector4_t *vb, const uint32_t *ib,
         view_forward.z = F16(-1.0f);
         view_forward.w = F16(1.0f);
 
-        /* VIEW_FORWARD in object space */
         fix16_vector4_t view_forward_object;
-        fix16_vector4_matrix4_multiply(&matrix_mv_inv, &view_forward, &view_forward_object);
+        fix16_vector4_matrix4_multiply(&matrix_wo, &view_forward,
+            &view_forward_object);
 
         uint32_t idx;
         for (idx = 0; idx < (ib_cnt * 4); idx += 4)
@@ -1965,24 +1964,34 @@ model_polygon_project(const fix16_vector4_t *vb, const uint32_t *ib,
                 /* Vertex C */ vtx[2] = &vb[ib[idx + 2]];
                 /* Vertex D */ vtx[3] = &vb[ib[idx + 3]];
 
-                /* Determine which to cull */
+#if CULLING == 1
+                /* Face culling */
                 fix16_t dot;
                 dot = fix16_vector4_dot(&nb[idx >> 2], &view_forward_object);
-
                 if (dot < F16(0.0f)) {
                         continue;
                 }
+#endif
 
-                fix16_vector4_matrix4_multiply(&matrix_mvp, vtx[0],
-                    &projected_polygon[0]);
-                fix16_vector4_matrix4_multiply(&matrix_mvp, vtx[1],
-                    &projected_polygon[1]);
-                fix16_vector4_matrix4_multiply(&matrix_mvp, vtx[3],
-                    &projected_polygon[3]);
-                fix16_vector4_matrix4_multiply(&matrix_mvp, vtx[2],
-                    &projected_polygon[2]);
+                fix16_vector4_matrix4_multiply(matrix_model_view, vtx[0],
+                    &vertex_mv[0]);
+                fix16_vector4_matrix4_multiply(matrix_model_view, vtx[1],
+                    &vertex_mv[1]);
+                fix16_vector4_matrix4_multiply(matrix_model_view, vtx[2],
+                    &vertex_mv[2]);
+                fix16_vector4_matrix4_multiply(matrix_model_view, vtx[3],
+                    &vertex_mv[3]);
 
-                ot_primitive_add(projected_polygon, color);
+                fix16_vector4_matrix4_multiply(matrix_projection, &vertex_mv[0],
+                    &vertex_projected[0]);
+                fix16_vector4_matrix4_multiply(matrix_projection, &vertex_mv[1],
+                    &vertex_projected[1]);
+                fix16_vector4_matrix4_multiply(matrix_projection, &vertex_mv[2],
+                    &vertex_projected[2]);
+                fix16_vector4_matrix4_multiply(matrix_projection, &vertex_mv[3],
+                    &vertex_projected[3]);
+
+                ot_primitive_add(vertex_mv, vertex_projected, color);
         }
 }
 
@@ -1991,14 +2000,25 @@ ot_init(void)
 {
         int32_t bucket_idx;
         for (bucket_idx = 0; bucket_idx < OT_PRIMITIVE_BUCKETS; bucket_idx++) {
-                TAILQ_INIT(&ot_primitive_buckets[bucket_idx]);
+                ot_bucket_init(bucket_idx);
         }
 
         ot_primitive_pool_idx = 0;
 }
 
 static void __unused
-ot_primitive_add(const fix16_vector4_t *proj_vertex, uint16_t color)
+ot_bucket_init(int32_t idx)
+{
+        struct ot_primitive_bucket *opb;
+        opb = &ot_primitive_bucket_pool[idx & (OT_PRIMITIVE_BUCKETS - 1)];
+
+        TAILQ_INIT(&opb->opb_bucket);
+        opb->opb_count = 0;
+}
+
+static void __unused
+ot_primitive_add(const fix16_vector4_t *vertex_mv,
+    const fix16_vector4_t *vertex_projected, uint16_t color)
 {
         struct ot_primitive *otp;
         otp = &ot_primitive_pool[ot_primitive_pool_idx];
@@ -2007,10 +2027,10 @@ ot_primitive_add(const fix16_vector4_t *proj_vertex, uint16_t color)
         ot_primitive_pool_idx &= OT_PRIMITIVE_CNT - 1;
 
         fix16_t avg;
-        avg = fix16_add(fix16_mul(fix16_add(
-                        fix16_add(proj_vertex[0].z, proj_vertex[1].z),
-                        fix16_add(proj_vertex[2].z, proj_vertex[3].z)),
-                F16(1.0f / 4.0f)), F16(0.01f));
+        avg = fix16_mul(fix16_add(
+                    fix16_add(vertex_mv[0].z, vertex_mv[1].z),
+                    fix16_add(vertex_mv[2].z, vertex_mv[3].z)),
+            F16(1.0f / 4.0f));
 
         otp->otp_color = color;
         otp->otp_avg = avg;
@@ -2021,39 +2041,40 @@ ot_primitive_add(const fix16_vector4_t *proj_vertex, uint16_t color)
         fix16_t half_height;
         half_height = F16((float)-SCREEN_HEIGHT / 2.0f);
 
-        otp->otp_coords[0].x = fix16_to_int(fix16_mul(proj_vertex[0].x,
-                half_width));
-        otp->otp_coords[0].y = fix16_to_int(fix16_mul(proj_vertex[0].y,
-                half_height));
-        otp->otp_coords[1].x = fix16_to_int(fix16_mul(proj_vertex[1].x,
-                half_width));
-        otp->otp_coords[1].y = fix16_to_int(fix16_mul(proj_vertex[1].y,
-                half_height));
-        otp->otp_coords[2].x = fix16_to_int(fix16_mul(proj_vertex[2].x,
-                half_width));
-        otp->otp_coords[2].y = fix16_to_int(fix16_mul(proj_vertex[2].y,
-                half_height));
-        otp->otp_coords[3].x = fix16_to_int(fix16_mul(proj_vertex[3].x,
-                half_width));
-        otp->otp_coords[3].y = fix16_to_int(fix16_mul(proj_vertex[3].y,
-                half_height));
+        otp->otp_coords[0].x = fix16_to_int(
+                fix16_mul(vertex_projected[0].x, half_width));
+        otp->otp_coords[0].y = fix16_to_int(
+                fix16_mul(vertex_projected[0].y, half_height));
+        otp->otp_coords[1].x = fix16_to_int(
+                fix16_mul(vertex_projected[1].x, half_width));
+        otp->otp_coords[1].y = fix16_to_int(
+                fix16_mul(vertex_projected[1].y, half_height));
+        otp->otp_coords[2].x = fix16_to_int(
+                fix16_mul(vertex_projected[2].x, half_width));
+        otp->otp_coords[2].y = fix16_to_int(
+                fix16_mul(vertex_projected[2].y, half_height));
+        otp->otp_coords[3].x = fix16_to_int(
+                fix16_mul(vertex_projected[3].x, half_width));
+        otp->otp_coords[3].y = fix16_to_int(
+                fix16_mul(vertex_projected[3].y, half_height));
         /* Screen coordinates */
 
-        fix16_t avg2;
-        avg2 = fix16_mul(fix16_abs(avg), F16(100.0f));
+        fix16_t abs_avg;
+        abs_avg = fix16_abs(avg);
 
         int32_t idx;
         /* XXX */
-        idx = fix16_to_int(avg2);
+        idx = fix16_to_int(abs_avg);
 
-        struct ot_primitive_buckets *otp_bucket;
-        otp_bucket = &ot_primitive_buckets[idx & (OT_PRIMITIVE_BUCKETS - 1)];
+        struct ot_primitive_bucket *opb;
+        opb = &ot_primitive_bucket_pool[idx & (OT_PRIMITIVE_BUCKETS - 1)];
 
-        TAILQ_INSERT_HEAD(otp_bucket, otp, otp_entries);
+        TAILQ_INSERT_HEAD(&opb->opb_bucket, otp, otp_entries);
+        opb->opb_count++;
 }
 
 static void __unused
-ot_primitive_bucket_sort(int32_t idx, int32_t type)
+ot_bucket_primitive_sort(int32_t idx, int32_t type)
 {
         struct ot_primitive *head;
         head = NULL;
@@ -2062,15 +2083,15 @@ ot_primitive_bucket_sort(int32_t idx, int32_t type)
         struct ot_primitive *otp;
 
         if (type == OT_PRIMITIVE_BUCKET_SORT_INSERTION) {
-                for (otp = TAILQ_FIRST(&ot_primitive_buckets[idx]);
+                for (otp = TAILQ_FIRST(&ot_primitive_bucket_pool[idx].opb_bucket);
                      (otp != NULL) && (safe = TAILQ_NEXT(otp, otp_entries), 1);
                      otp = safe) {
                         struct ot_primitive *otp_current;
                         otp_current = otp;
-                        if ((head == NULL) || (otp_current->otp_avg < head->otp_avg)) {
+                        if ((head == NULL) || (otp_current->otp_avg > head->otp_avg)) {
                                 TAILQ_NEXT(otp_current, otp_entries) = head;
                                 head = otp_current;
-                                TAILQ_FIRST(&ot_primitive_buckets[idx]) = head;
+                                TAILQ_FIRST(&ot_primitive_bucket_pool[idx].opb_bucket) = head;
                                 continue;
                         }
 
@@ -2080,7 +2101,7 @@ ot_primitive_bucket_sort(int32_t idx, int32_t type)
                                 otp_p_next = &TAILQ_NEXT(otp_p, otp_entries);
 
                                 if ((*otp_p_next == NULL) ||
-                                    (otp_current->otp_avg < (*otp_p_next)->otp_avg)) {
+                                    (otp_current->otp_avg > (*otp_p_next)->otp_avg)) {
                                         TAILQ_NEXT(otp_current, otp_entries) =
                                             *otp_p_next;
                                         *otp_p_next = otp_current;
@@ -2098,7 +2119,7 @@ ot_primitive_bucket_sort(int32_t idx, int32_t type)
                         struct ot_primitive *otp_k __unused;
                         otp_k = NULL;
 
-                        head = TAILQ_FIRST(&ot_primitive_buckets[idx]);
+                        head = TAILQ_FIRST(&ot_primitive_bucket_pool[idx].opb_bucket);
                         for (otp = head;
                              (otp != NULL) && (safe = TAILQ_NEXT(otp, otp_entries), 1);
                              otp = safe) {
@@ -2112,9 +2133,9 @@ ot_primitive_bucket_sort(int32_t idx, int32_t type)
                                         continue;
                                 }
 
-                                if (otp_j->otp_avg < otp_i->otp_avg) {
+                                if (otp_j->otp_avg > otp_i->otp_avg) {
                                         if (otp_k == NULL) {
-                                                TAILQ_FIRST(&ot_primitive_buckets[idx]) =
+                                                TAILQ_FIRST(&ot_primitive_bucket_pool[idx].opb_bucket) =
                                                     otp_j;
                                         } else {
                                                 TAILQ_NEXT(otp_k, otp_entries) =
@@ -2131,5 +2152,6 @@ ot_primitive_bucket_sort(int32_t idx, int32_t type)
                                 otp_k = otp_i;
                         }
                 } while (swapped);
+        } else if (type == OT_PRIMITIVE_BUCKET_SORT_QUICK) {
         }
 }
