c604a7c2897aa6d929cdd34d5ea891f9f4bf8d1c
Israel Jacquez <mrkotfw@gmail.com>
2015-08-23 15:22:12 -0700

    Turn display on AFTER copying buffer to VRAM

--- libyaul/scu/bus/b/vdp2/vdp2_scrn_back_screen_set.c
+++ libyaul/scu/bus/b/vdp2/vdp2_scrn_back_screen_set.c
@@ -16,27 +16,16 @@ void
 vdp2_scrn_back_screen_set(bool single_color, uint32_t vram, uint16_t *color,
     uint16_t len)
 {
-
+        /* Wait for the start of VBLANK-IN */
         vdp2_tvmd_vblank_in_wait();
 
         MEMORY_WRITE(16, VDP2(BKTAU),
             (single_color ? 0x0000 : 0x8000) | ((vram >> 17) & 0x03));
         MEMORY_WRITE(16, VDP2(BKTAL), (vram >> 1) & 0xFFFF);
 
-        /* Force display. If BDCLMD is set and DISP has never been set,
-         * the back screen will not display properly. */
-        vdp2_tvmd_display_set();
-
-        uint16_t tvmd;
-
-        tvmd = MEMORY_READ(16, VDP2(TVMD));
-        tvmd &= 0xFEFF;
-        tvmd |= 0x0100;
-
-        MEMORY_WRITE(16, VDP2(TVMD), tvmd);
-
         if (single_color) {
                 assert(len == 1);
+
                 MEMORY_WRITE(16, vram, *color);
         } else {
                 /* Make sure that the number of lines to draw is a
@@ -48,4 +37,15 @@ vdp2_scrn_back_screen_set(bool single_color, uint32_t vram, uint16_t *color,
                         MEMORY_WRITE(16, (vram + (idx << 1)), *color++);
                 }
         }
+
+        /* Force display. If BDCLMD is set and DISP has never been set,
+         * the back screen will not display properly. */
+        vdp2_tvmd_display_set();
+
+        uint16_t tvmd;
+        tvmd = MEMORY_READ(16, VDP2(TVMD));
+        tvmd &= 0xFEFF;
+        tvmd |= 0x0100;
+
+        MEMORY_WRITE(16, VDP2(TVMD), tvmd);
 }
