c1e1523fed7ad8b621425b2931c8ce89389a6f7c
Israel Jacquez <mrkotfw@gmail.com>
2016-01-26 00:48:21 -0800

    Remove hard tabs in .h file

--- libyaul/scu/bus/b/vdp1/vdp1/map.h
+++ libyaul/scu/bus/b/vdp1/vdp1/map.h
@@ -15,16 +15,16 @@
 
 /* Helpers specific to this processor */
 
-#define TVMR		0x0000
-#define FBCR		0x0002
-#define PTMR		0x0004
-#define EWDR		0x0006
-#define EWLR		0x0008
-#define EWRR		0x000A
-#define ENDR		0x000C
-#define EDSR		0x0010
-#define LOPR		0x0012
-#define COPR		0x0014
-#define MODR		0x0016
+#define TVMR            0x0000
+#define FBCR            0x0002
+#define PTMR            0x0004
+#define EWDR            0x0006
+#define EWLR            0x0008
+#define EWRR            0x000A
+#define ENDR            0x000C
+#define EDSR            0x0010
+#define LOPR            0x0012
+#define COPR            0x0014
+#define MODR            0x0016
 
 #endif /* !_VDP1_MAP_H_ */
