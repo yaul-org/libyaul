8e7d395786fc6a6521b27711721dfc98df938316
Israel Jacquez <mrkotfw@gmail.com>
2016-05-20 01:04:43 -0700

    Remove examples/simple/arp-msg
    For now, it shouldn't be present as it's broken.

--- examples/simple/arp-msg/host/arp.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARP_H
-#define ARP_H
-
-int arp_open(void);
-void arp_close(void);
-int arp_read(void *buf, unsigned addr, unsigned size);
-int arp_write(void *buf, unsigned addr, unsigned size, int boot);
-
-#endif
