436a4cc908c937efd85ee7eb82542ed56ce4bc65
Israel Jacquez <mrkotfw@gmail.com>
2017-09-17 00:12:59 -0700

    Add temporary stub for snprintf()

--- libyaul/lib/stdio/snprintf.c
+++ libyaul/lib/stdio/snprintf.c
@@ -3,5 +3,9 @@
 int
 snprintf(char *__restrict str, size_t size, const char *__restrict format, ...)
 {
+        if (str != NULL) {
+                memcpy(str, format, strlen(format) + 1);
+        }
+
         return 0;
 }
