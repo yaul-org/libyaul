d224e083d0d05de47fdb763ace10b8060f292e97
Israel Jacquez <mrkotfw@gmail.com>
2018-07-13 20:23:47 -0700

    Allow malloc() and free() to be overwritten
    This allows another allocator to be used instead of SLOB.

--- libyaul/lib/stdlib/free.c
+++ libyaul/lib/stdlib/free.c
@@ -1,7 +1,13 @@
 #include <mm/slob.h>
 
+void __attribute__ ((weak, alias("free")))
+override_free(void *addr)
+{
+        slob_free(addr);
+}
+
 void
 free(void *addr)
 {
-        slob_free(addr);
+        override_free(addr);
 }
