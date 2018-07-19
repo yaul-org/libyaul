642e89288c783f29850b6d205bb52d56487eeb19
Israel Jacquez <mrkotfw@gmail.com>
2018-07-18 21:24:12 -0700

    Implement CPU DIVU functionality

---
+++ libyaul/scu/bus/cpu/cpu/instructions.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2012-2016 Israel Jacquez
+ * See LICENSE for details.
+ *
+ * Israel Jacquez <mrkotfw@gmail.com>
+ */
+
+#ifndef _CPU_INSTRUCTIONS_H_
+#define _CPU_INSTRUCTIONS_H_
+
+#include <sys/cdefs.h>
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+static inline void __attribute__ ((always_inline))
+cpu_instr_clrmac(void)
+{
+        __asm__ volatile ("clrmac"
+            : /* No outputs */
+            : /* No inputs */
+            : "mach", "macl");
+}
+
+static inline uint32_t __attribute__ ((always_inline))
+cpu_instr_xtrct(uint32_t rm, uint32_t rn)
+{
+        __asm__ volatile ("xtrct %[rm], %[rn]"
+            : [rn] "=&r" (rn)
+            : "0" (rn), [rm] "r" (rm));
+
+        return rn;
+}
+
+static inline void __attribute__ ((always_inline))
+cpu_instr_nop(void)
+{
+        __asm__ volatile ("nop"
+            : /* No outputs */
+            : /* No inputs */
+        );
+}
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* !_CPU_INSTRUCTIONS_H_ */
---
+++ libyaul/scu/bus/cpu/cpu_divu.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2012-2016
+ * See LICENSE for details.
+ *
+ * Israel Jacquez <mrkotfw@gmail.com
+ */
+
+#include <sys/cdefs.h>
+
+#include <smpc/smc.h>
+
+#include <cpu/divu.h>
+#include <cpu/frt.h>
+#include <cpu/intc.h>
+#include <cpu/map.h>
+
+static void _divu_ovfi_handler(void);
+
+static void _default_ihr(void);
+
+static void (*_divu_ovfi_ihr)(void) = _default_ihr;
+
+void
+cpu_divu_init(void)
+{
+        cpu_divu_ovfi_clear();
+
+        cpu_intc_ihr_set(INTC_INTERRUPT_DIVU_OVFI, _divu_ovfi_handler);
+}
+
+void
+cpu_divu_ovfi_set(void (*ihr)(void))
+{
+        volatile uint32_t *reg_dvcr;
+        reg_dvcr = (volatile uint32_t *)CPU(DVCR);
+
+        *reg_dvcr &= ~0x00000003;
+
+        _divu_ovfi_ihr = _default_ihr;
+
+        if (ihr != NULL) {
+                _divu_ovfi_ihr = ihr;
+
+                *reg_dvcr |= 0x00000002;
+        }
+}
+
+static void __attribute__ ((interrupt_handler))
+_divu_ovfi_handler(void)
+{
+        MEMORY_WRITE_AND(32, CPU(DVCR), ~0x00000001);
+
+        _divu_ovfi_ihr();
+}
+
+static void
+_default_ihr(void)
+{
+}
