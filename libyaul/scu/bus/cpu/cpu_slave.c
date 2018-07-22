b32039d9839f83b0e05c124d896f0a144f8e0db6
Israel Jacquez <mrkotfw@gmail.com>
2018-07-21 23:51:14 -0700

    Allow master CPU to receive notification via input capture interrupt

--- libyaul/scu/bus/cpu/cpu_slave.c
+++ libyaul/scu/bus/cpu/cpu_slave.c
@@ -17,41 +17,89 @@
 
 static void _slave_entry(void);
 
+static void _ici_handler(void);
+
 static void _default_entry(void);
+static void _default_ihr(void);
 
 static void (*_entry)(void) = _default_entry;
+
+static void (*_ici_ihr)(void) = _default_ihr;
 
 void
 cpu_slave_init(void)
 {
         smpc_smc_sshoff_call();
 
-        cpu_slave_entry_clear();
+        cpu_slave_entry_clear(CPU_MASTER);
+        cpu_slave_entry_clear(CPU_SLAVE);
 
         cpu_intc_ihr_set(INTC_INTERRUPT_SLAVE_ENTRY, _slave_entry);
+
+        cpu_intc_ihr_set(INTC_INTERRUPT_FRT_ICI, _ici_handler);
 
         smpc_smc_sshon_call();
 }
 
 void
-cpu_slave_entry_set(void (*entry)(void))
+cpu_slave_entry_set(uint8_t cpu, void (*entry)(void))
 {
-        _entry = (entry != NULL) ? entry : _default_entry;
+        switch (cpu & 0x01) {
+        case CPU_MASTER: {
+                volatile uint8_t *reg_tier;
+                reg_tier = (volatile uint8_t *)CPU(TIER);
+
+                *reg_tier &= ~0x80;
+
+                MEMORY_WRITE_AND(8, CPU(FTCSR), ~0x80);
+
+                _ici_ihr = _default_ihr;
+
+                if (entry != NULL) {
+                        _ici_ihr = entry;
+
+                        *reg_tier |= 0x80;
+                }
+        } break;
+        case CPU_SLAVE:
+                _entry = (entry != NULL) ? entry : _default_entry;
+                break;
+        }
 }
 
 static void __noreturn
 _slave_entry(void)
 {
-        while (true) {
-                while (((cpu_frt_status_get()) & FRTCS_ICF) == 0x00);
+        MEMORY_WRITE_AND(8, CPU(TIER), ~0x80);
 
-                cpu_frt_control_chg((uint8_t)~FRTCS_ICF);
+        while (true) {
+                cpu_slave_notification_wait();
 
                 _entry();
         }
 }
 
+static void __attribute__ ((interrupt_handler))
+_ici_handler(void)
+{
+        volatile uint8_t *reg_tier;
+        reg_tier = (volatile uint8_t *)CPU(TIER);
+
+        *reg_tier &= ~0x80;
+
+        MEMORY_WRITE_AND(8, CPU(FTCSR), ~0x80);
+
+        _ici_ihr();
+
+        *reg_tier |= 0x80;
+}
+
 static void
 _default_entry(void)
+{
+}
+
+static void
+_default_ihr(void)
 {
 }
