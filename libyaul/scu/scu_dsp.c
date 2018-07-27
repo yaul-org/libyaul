f809da5be28f7a22641dad84d1752691a7d211b2
Israel Jacquez <mrkotfw@gmail.com>
2018-07-26 22:43:51 -0700

    Add support for fetching SCU DSP status
    This should also take into account the destructive readout of the
    SCU(PPAF) register.

--- libyaul/scu/scu_dsp.c
+++ libyaul/scu/scu_dsp.c
@@ -15,8 +15,8 @@ static void _dsp_end_handler(void);
 static void _default_ihr(void);
 
 /* XXX: State that should be moved (eventually) */
+static bool _overflow = false;
 static bool _end = true;
-static bool _stepping = false;
 
 static void (*_dsp_end_ihr)(void) = _default_ihr;
 
@@ -28,11 +28,13 @@ scu_dsp_init(void)
 
         scu_dsp_program_stop();
 
+        scu_dsp_program_clear();
+
         scu_dsp_end_clear();
 
         scu_ic_ihr_set(IC_INTERRUPT_DSP_END, _dsp_end_handler);
 
-        _stepping = false;
+        _overflow = false;
         _end = true;
 }
 
@@ -76,11 +78,25 @@ scu_dsp_program_load(const void *program, uint32_t count)
 }
 
 void
+scu_dsp_program_clear(void)
+{
+        scu_dsp_program_stop();
+
+        uint32_t i;
+        for (i = 0; i < DSP_PROGRAM_WORD_COUNT; i++) {
+                MEMORY_WRITE(32, SCU(PPD), 0xF8000000);
+        }
+
+        /* Set DSP's program counter to 0 */
+        MEMORY_WRITE(32, SCU(PPAF), 0x00008000);
+}
+
+void
 scu_dsp_program_pc_set(uint8_t pc)
 {
         MEMORY_WRITE(32, SCU(PPAF), 0x00008000 | pc);
 
-        _stepping = false;
+        _overflow = false;
         _end = true;
 }
 
@@ -89,7 +105,7 @@ scu_dsp_program_start(void)
 {
         MEMORY_WRITE(32, SCU(PPAF), 0x00010000);
 
-        _stepping = false;
+        _overflow = false;
         _end = false;
 }
 
@@ -98,7 +114,7 @@ scu_dsp_program_stop(void)
 {
         MEMORY_WRITE(32, SCU(PPAF), 0x00000000);
 
-        _stepping = false;
+        _overflow = false;
         _end = true;
 }
 
@@ -114,17 +130,14 @@ scu_dsp_program_step(void)
         uint8_t pc;
         pc = ppaf_bits & 0xFF;
 
-        /* Start stepping */
-        _stepping = true;
-
         *reg_ppaf = 0x00020000;
 
         while (true) {
+                _overflow = (ppaf_bits & 0x00080000) != 0x00000000;
+
                 /* Read SCU(PPAF) as the program end interrupt flag is
                  * reset when read */
                 _end = (ppaf_bits & 0x00040000) != 0x00000000;
-                /* We're no longer stepping if we've reached the end */
-                _stepping = !_end;
 
                 /* Wait until DSP is done executing one instruction */
                 if (_end || ((ppaf_bits & 0x00030000) == 0x00000000)) {
@@ -140,11 +153,23 @@ scu_dsp_program_step(void)
 bool
 scu_dsp_program_end(void)
 {
-        if (_stepping) {
-                return _end;
+        if (_end) {
+                return true;
         }
 
-        return ((MEMORY_READ(32, SCU(PPAF)) & 0x00040000) == 0x00000000);
+        volatile uint32_t *reg_ppaf;
+        reg_ppaf = (volatile uint32_t *)SCU(PPAF);
+
+        uint32_t ppaf_bits;
+        ppaf_bits = *reg_ppaf;
+
+        if (!_overflow) {
+                _overflow = (ppaf_bits & 0x00080000) != 0x00000000;
+        }
+
+        _end = (ppaf_bits & 0x00040000) != 0x00000000;
+
+        return _end;
 }
 
 void
@@ -215,6 +240,28 @@ scu_dsp_data_write(uint8_t ram_page, uint8_t offset, void *data, uint32_t count)
 
                 MEMORY_WRITE(32, SCU(PDD), data_p[i]);
         }
+}
+
+void
+scu_dsp_status_get(struct dsp_status *status)
+{
+        if (status == NULL) {
+                return;
+        }
+
+        volatile uint32_t *reg_ppaf;
+        reg_ppaf = (volatile uint32_t *)SCU(PPAF);
+
+        uint32_t ppaf_bits;
+        ppaf_bits = *reg_ppaf;
+
+        uint32_t *status_p;
+        status_p = (uint32_t *)status;
+
+        *status_p = ppaf_bits;
+
+        status->v = status->v || _overflow;
+        status->e = status->e || _end;
 }
 
 static void
