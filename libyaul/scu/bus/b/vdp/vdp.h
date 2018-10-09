02c9036321a737ab77a3ab3e57477231d8448820
Israel Jacquez <mrkotfw@gmail.com>
2018-10-09 09:26:25 -0700

    Extern vdp_*() functions

--- libyaul/scu/bus/b/vdp/vdp.h
+++ libyaul/scu/bus/b/vdp/vdp.h
@@ -11,6 +11,10 @@
 #include <vdp1.h>
 #include <vdp2.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 extern void vdp_init(void);
 
 extern void vdp_sync_init(void);
@@ -19,5 +23,9 @@ extern void vdp1_sync_draw(const struct vdp1_cmdt *, uint16_t);
 extern void vdp1_sync_draw_wait(void);
 extern void vdp2_sync_commit(void);
 extern void vdp2_sync_commit_wait(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* !_VDP_H_ */
