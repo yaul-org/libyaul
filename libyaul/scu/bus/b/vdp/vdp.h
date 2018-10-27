177eb89422bec6f7ff31f132073be7facaa2b67b
Israel Jacquez <mrkotfw@gmail.com>
2018-10-27 09:06:19 -0700

    Sort funcion prototypes

--- libyaul/scu/bus/b/vdp/vdp.h
+++ libyaul/scu/bus/b/vdp/vdp.h
@@ -27,10 +27,12 @@ extern void vdp_init(void);
 
 extern void vdp_sync_init(void);
 extern void vdp_sync(int16_t);
+
 extern void vdp1_sync_draw(const struct vdp1_cmdt_list *);
 extern void vdp1_sync_draw_wait(void);
-extern void vdp2_sync_commit(void);
 extern uint16_t vdp1_sync_last_command_get(void);
+
+extern void vdp2_sync_commit(void);
 
 extern void vdp_sync_vblank_in_set(void (*)(void));
 extern void vdp_sync_vblank_out_set(void (*)(void));
