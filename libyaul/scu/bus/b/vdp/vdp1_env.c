7032f2c0f29c9cae207ef00ade7727209f0fd1cd
Israel Jacquez <mrkotfw@gmail.com>
2018-10-27 16:52:37 -0700

    Set VDP1 BPP and rotation bits to VDP1(TVMR)

--- libyaul/scu/bus/b/vdp/vdp1_env.c
+++ libyaul/scu/bus/b/vdp/vdp1_env.c
@@ -17,16 +17,21 @@ vdp1_env_set(const struct vdp1_env *env)
 {
         assert(env != NULL);
 
+        assert((env->env_bpp == ENV_BPP_8) || (env->env_bpp == ENV_BPP_16));
+
+        assert((env->env_rotation == ENV_ROTATION_0) ||
+               (env->env_rotation == ENV_ROTATION_90));
+
         assert((env->env_erase_points[0].x >= 0) &&
                (env->env_erase_points[0].y >= 0));
 
-        assert((env->env_erase_points[1].x >= 0) &&
+        assert((env->env_erase_points[1].x > 0) &&
                (env->env_erase_points[1].y >= 0));
 
-        assert((env->env_erase_points[0].x < 1024) &&
+        assert((env->env_erase_points[0].x <= 1008) &&
                (env->env_erase_points[0].y < 512));
 
-        assert((env->env_erase_points[1].x < 1024) &&
+        assert((env->env_erase_points[1].x <= 1024) &&
                (env->env_erase_points[1].y < 512));
 
         assert((env->env_erase_points[0].x < env->env_erase_points[1].x) &&
@@ -35,12 +40,28 @@ vdp1_env_set(const struct vdp1_env *env)
         assert((env->env_color_mode == ENV_COLOR_MODE_PALETTE) ||
                (env->env_color_mode == ENV_COLOR_MODE_RGB_PALETTE));
 
+        /* Always clear TVM and VBE bits */
+        _state_vdp1()->regs.tvmr = (env->env_rotation << 1) | env->env_bpp;
+
         _state_vdp1()->regs.ewdr = env->env_color.raw;
 
-        _state_vdp1()->regs.ewlr = ((env->env_erase_points[0].x >> 3) << 9) |
-                                   env->env_erase_points[0].y;
-        _state_vdp1()->regs.ewrr = (((env->env_erase_points[1].x - 1) >> 3) << 9) |
-                                   env->env_erase_points[1].y;
+        uint16_t x1;
+        x1 = env->env_erase_points[0].x >> 3;
+        uint16_t x3;
+        x3 = env->env_erase_points[1].x >> 3;
+
+        uint16_t y1;
+        y1 = env->env_erase_points[0].y;
+        uint16_t y3;
+        y3 = env->env_erase_points[1].y;
+
+        if (env->env_bpp == ENV_BPP_8) {
+                x1 >>= 1;
+                x3 >>= 1;
+        }
+
+        _state_vdp1()->regs.ewlr = (x1 << 9) | y1;
+        _state_vdp1()->regs.ewrr = (x3 << 9) | y3;
 
         _state_vdp2()->regs.spctl &= 0x371F;
         _state_vdp2()->regs.spctl |= env->env_color_mode << 5;
