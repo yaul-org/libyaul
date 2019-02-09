0c64510a142efde0dea6fd0b43fd5eceb58d2c1d
Israel Jacquez <mrkotfw@gmail.com>
2019-02-09 14:28:37 -0800

    Add new operator overloads from new C++ standards (C++11 on onward)

--- libyaul/common/c++-support.cxx
+++ libyaul/common/c++-support.cxx
@@ -15,10 +15,23 @@ void* __weak operator new[](size_t size) {
     return malloc(size);
 }
 
-void __weak operator delete(void *ptr) {
+void __weak operator delete(void* ptr) {
     free(ptr);
 }
 
-void __weak operator delete[](void *ptr) {
+void __weak operator delete[](void* ptr) {
+    free(ptr);
+}
+
+/*-
+ * <https://en.cppreference.com/w/cpp/memory/new/operator_delete>
+ *
+ * Called if a user-defined replacement is provided, except that it's
+ * unspecified whether other overloads or this overload is called when deleting
+ * objects of incomplete type and arrays of non-class and trivially-destructible
+ * class types.
+ *
+ * A memory allocator can use the given size to be more efficient */
+void __weak operator delete(void* ptr, unsigned int) {
     free(ptr);
 }
