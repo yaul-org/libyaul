95e4d18e9a36366dbc02be8c4bd21d1bdc049fda
Israel Jacquez <mrkotfw@gmail.com>
2019-02-11 21:33:20 -0800

    Add memory VDP1 VRAM macros to access VRAM and back frame buffer

--- libyaul/scu/bus/b/vdp/vdp1/vram.h
+++ libyaul/scu/bus/b/vdp/vdp1/vram.h
@@ -17,6 +17,10 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#define VDP1_VRAM(x)            (0x25C00000 + (x))
+#define VDP1_FRAME_BUFFER(x, y) (0x25C80000 + (((x) & 0x1) << 18) + (y))
+#define VDP1_CMD_TABLE(x, y)    (0x25C00000 + ((x) << 5) + (((y) << 1) & 0x1F))
+
 #define VDP1_VRAM_SIZE 0x00080000 /* In bytes */
 
 struct vdp1_gouraud_table {
