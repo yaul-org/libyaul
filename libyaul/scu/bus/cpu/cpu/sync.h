0dbabf8dcc9786b5e5c0a939f923eda6c67844c7
Israel Jacquez <mrkotfw@gmail.com>
2019-06-28 13:10:15 -0700

    Add cpu_sync_mutex() and cpu_sync_mutex_clear()

--- libyaul/scu/bus/cpu/cpu/sync.h
+++ libyaul/scu/bus/cpu/cpu/sync.h
@@ -16,27 +16,62 @@
 extern "C" {
 #endif /* __cplusplus */
 
+static inline bool __always_inline
+cpu_sync_mutex(uint8_t b)
+{
+        register uint8_t *bios_address;
+        bios_address = (uint8_t *)0x26000B00;
+
+        register uint32_t result;
+        result = 0;
+
+        /* Currently using cpu_sync_mutex() to compare the value results in
+         * extra instructions being emitted. Because TAS.B sets the T bit in SR,
+         * there shouldn't be a need for the MOVT instruction */
+
+        __asm__ volatile ("\tadd %[b], %[bios_address]\n"
+                          "\ttas.b @%[out]\n"
+                          "\tmovt %[result]"
+            /* Output */
+            : [out] "=r" (bios_address),
+              [result] "=r" (result)
+            /* Input */
+            : [bios_address] "r" (bios_address),
+              [b] "r" (b));
+
+        return result;
+}
+
+static inline void __always_inline
+cpu_sync_mutex_clear(uint8_t b)
+{
+        register uint8_t *bios_address;
+        bios_address = (uint8_t *)0x26000B00;
+
+        *(bios_address + b) = 0x00;
+}
+
 static inline void __always_inline
 cpu_sync_spinlock(uint8_t b)
 {
         register uint8_t *bios_address;
         bios_address = (uint8_t *)0x26000B00;
 
-        __asm__ volatile ("add %[b], %[bios_address]\n"
+        __asm__ volatile ("\tadd %[b], %[bios_address]\n"
                           "1:\n"
                           "\ttas.b @%[out]\n"
                           "\tbf 1b"
+            /* Output */
             : [out] "=r" (bios_address)
-            : [bios_address] "0" (bios_address), [b] "r" (b));
+            /* Input */
+            : [bios_address] "0" (bios_address),
+              [b] "r" (b));
 }
 
 static inline void __always_inline
 cpu_sync_spinlock_clear(uint8_t b)
 {
-        register uint8_t *bios_address;
-        bios_address = (uint8_t *)0x26000B00;
-
-        *(bios_address + b) = 0x00;
+        cpu_sync_mutex_clear(b);
 }
 
 #ifdef __cplusplus
