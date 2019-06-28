62ab17f03d95b6183c9758a501dae1d0f2851aed
Israel Jacquez <mrkotfw@gmail.com>
2019-06-28 14:49:07 -0700

    Remove literal tabs in libyaul/lib/

--- libyaul/lib/lib/assert.h
+++ libyaul/lib/lib/assert.h
@@ -1,5 +1,7 @@
 #ifndef _ASSERT_H_
-#define	_ASSERT_H_
+#define _ASSERT_H_
+
+#include <sys/cdefs.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,34 +11,24 @@ extern "C" {
 
 #undef assert
 
-#ifdef NDEBUG /* required by ANSI standard */
-#define assert(__e) ((void)0)
-#else
 #define assert(__e) ((__e) ? (void)0 : __assert_func(__FILE__, __LINE__,       \
         __ASSERT_FUNC, #__e))
 
 #ifndef __ASSERT_FUNC
-/* Use g++'s demangled names in C++. */
-#if defined __cplusplus && defined __GNUC__
+/* Use g++'s demangled names in C++ */
+#if defined(__cplusplus) && defined(__GNUC__)
 #define __ASSERT_FUNC __PRETTY_FUNCTION__
-
-/* C99 requires the use of __func__. */
+/* C99 requires the use of __func__ */
 #elif __STDC_VERSION__ >= 199901L
 #define __ASSERT_FUNC __func__
-
-/* Older versions of gcc don't have __func__ but can use __FUNCTION__. */
-#elif __GNUC__ >= 2
-#define __ASSERT_FUNC __FUNCTION__
-
-/* Failed to detect __func__ support. */
+/* Failed to detect __func__ support */
 #else
 #define __ASSERT_FUNC ((char *)0)
 #endif
 #endif /* !__ASSERT_FUNC */
-#endif /* !NDEBUG */
 
-void __assert(const char *, int, const char *) __attribute__ ((__noreturn__));
-void __assert_func(const char *, int, const char *, const char *) __attribute__ ((__noreturn__));
+void __assert(const char *, int, const char *) __noreturn;
+void __assert_func(const char *, int, const char *, const char *) __noreturn;
 
 #ifdef __cplusplus
 }
--- libyaul/lib/lib/string.h
+++ libyaul/lib/lib/string.h
@@ -1,5 +1,5 @@
 #ifndef _STRING_H_
-#define	_STRING_H_
+#define _STRING_H_
 
 #include "_ansi.h"
 
