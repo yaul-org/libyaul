9c3d46ea32aee2a3b266fdd02dedc3924148fa99
Israel Jacquez <mrkotfw@gmail.com>
2019-06-29 19:41:59 -0700

    Add new attribute

--- libyaul/lib/lib/sys/cdefs.h
+++ libyaul/lib/lib/sys/cdefs.h
@@ -111,6 +111,7 @@
 #define __unused                __attribute__ ((__unused__))
 #define __used                  __attribute__ ((__used__))
 #define __weak                  __attribute__ ((__weak__))
+#define __leaf                  __attribute__ ((leaf))
 
 #if __has_attribute(__alloc_align__)
 #define __alloc_align(x)        __attribute__ ((__alloc_align__ (x)))
