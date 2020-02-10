7064af7c17cc550ddc35a8470ca968f7787e4802
Israel Jacquez <mrkotfw@gmail.com>
2020-02-09 22:17:21 -0800

    Add basic documentation for SCU-Timer

--- libyaul/scu/scu/timer.h
+++ libyaul/scu/scu/timer.h
@@ -5,6 +5,8 @@
  * Israel Jacquez <mrkotfw@gmail.com>
  */
 
+/// @defgroup SCU_TIMER
+
 #ifndef _SCU_TIMER_H_
 #define _SCU_TIMER_H_
 
@@ -15,48 +17,83 @@
 
 __BEGIN_DECLS
 
+/// @addtogroup SCU_TIMER_INLINE_FUNCTIONS
+/// @{
+
+/// @function
+/// @brief Not yet documented.
 static inline void __always_inline
 scu_timer_t0_value_set(uint16_t value)
 {
         MEMORY_WRITE(32, SCU(T0C), value & 0x03FF);
 }
 
+/// @function
+/// @brief Not yet documented.
 static inline void __always_inline
 scu_timer_t1_value_set(uint16_t value)
 {
         MEMORY_WRITE(32, SCU(T1S), value & 0x01FF);
 }
 
+/// @function
+/// @brief Not yet documented.
 static inline void __always_inline
 scu_timer_disable(void)
 {
         MEMORY_WRITE(32, SCU(T1MD), 0x00000000);
 }
 
+/// @function
+/// @brief Not yet documented.
 static inline void __always_inline
 scu_timer_enable(void)
 {
         MEMORY_WRITE(32, SCU(T1MD), 0x00000001);
 }
 
+/// @function
+/// @brief Not yet documented.
 static inline void __always_inline
 scu_timer_line_enable(void)
 {
         MEMORY_WRITE(32, SCU(T1MD), 0x00000101);
 }
 
+/// @macro
+/// @brief Not yet documented.
 #define scu_timer_t0_clear() do {                                              \
         scu_timer_t0_set(NULL);                                                \
 } while (false)
 
+/// @macro
+/// @brief Not yet documented.
 #define scu_timer_t1_clear() do {                                              \
         scu_timer_t1_set(NULL);                                                \
 } while (false)
 
+/// @}
+
+/// @addtogroup SCU_TIMER_FUNCTIONS
+/// @{
+
+/// @function
+/// @brief Not yet documented.
 extern void scu_timer_init(void);
+
+/// @function
+/// @brief Not yet documented.
 extern void scu_timer_t0_set(void (*)(void));
+
+/// @function
+/// @brief Not yet documented.
 extern void scu_timer_t1_set(void (*)(void));
+
+/// @function
+/// @brief Not yet documented.
 extern void scu_timer_t1_line_set(int16_t);
+
+/// @}
 
 __END_DECLS
 
