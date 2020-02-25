aae17a2ec7801a2ca3b8f5212fc4f4b24157deb6
Israel Jacquez <mrkotfw@gmail.com>
2020-02-24 22:28:51 -0800

    Specify correct path

--- libyaul/common/post.common.mk
+++ libyaul/common/post.common.mk
@@ -190,7 +190,7 @@ $(SH_PROGRAM).iso: $(SH_PROGRAM).bin IP.BIN $(shell find $(IMAGE_DIRECTORY)/ -ty
 	$(ECHO)$(YAUL_INSTALL_ROOT)/bin/make-iso $(IMAGE_DIRECTORY) $(SH_PROGRAM) $(MAKE_ISO_REDIRECT)
 
 $(SH_PROGRAM).cue: $(SH_PROGRAM).iso
-	$(ECHO)$(YAUL_INSTALL_ROOT)/bin/make-cue "$(MAKE_ISO_REDIRECT).iso"
+	$(ECHO)$(YAUL_INSTALL_ROOT)/bin/make-cue "$(SH_PROGRAM).iso" $(MAKE_ISO_REDIRECT)
 
 IP.BIN: $(YAUL_INSTALL_ROOT)/share/yaul/bootstrap/ip.sx
 	$(ECHO)$(YAUL_INSTALL_ROOT)/bin/make-ip	\
