98b63bf29987874be567e3210e23dd9057d1aa6a
Israel Jacquez <mrkotfw@gmail.com>
2020-04-24 14:36:51 -0700

    Add files to ignore specific directories

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+;; ((projectile-globally-ignored-files . (append projectile-globally-ignored-files '(".meta" ".asset"))))
+((nil . ((magit-todos-depth . 1)
+         (projectile-git-command . "fdfind . --color=never --type f -0 -H -E .git --ignore-file .projectile-ignore"))))
