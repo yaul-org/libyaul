7741d5b3e83ee861290cf67c294e8723137b83b4
Israel Jacquez <mrkotfw@gmail.com>
2020-10-21 22:41:59 -0700

    Remove SLOB completely

--- libyaul/lib/stdlib/realloc.c
+++ libyaul/lib/stdlib/realloc.c
@@ -2,9 +2,7 @@
 
 #if defined(MALLOC_IMPL_TLSF)
 #include <mm/tlsf.h>
-#elif defined(MALLOC_IMPL_SLOB)
-#include <mm/slob.h>
-#endif /* MALLOC_IMPL_TLSF || MALLOC_IMPL_SLOB */
+#endif /* MALLOC_IMPL_TLSF */
 
 #include <internal.h>
 
@@ -18,9 +16,7 @@ realloc(void *old, size_t new_len)
         pool = master_state()->tlsf_pools[TLSF_POOL_PRIVATE];
 
         ret = tlsf_realloc(pool, old, new_len);
-#elif defined(MALLOC_IMPL_SLOB)
-        ret = slob_realloc(old, new_len);
-#endif /* MALLOC_IMPL_TLSF || MALLOC_IMPL_SLOB */
+#endif /* MALLOC_IMPL_TLSF */
 
         return ret;
 }
