aca7b78be5f39dbd2fc663540eb05c2870ff64f6
Israel Jacquez <mrkotfw@gmail.com>
2020-10-31 15:50:55 -0700

    Update Emacs related project files

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,1 @@
-((nil . ((magit-todos-depth . 1)
-         (projectile-git-command . "fdfind . --color=never --type f -0 -H -E .git --ignore-file .projectile-ignore"))))
+((nil . ((magit-todos-depth . 1))))
