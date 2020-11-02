29d0bfef5fd2f9ca41c6919ab21e58eaf9f91572
Israel Jacquez <mrkotfw@gmail.com>
2020-11-02 00:37:48 -0800

    Split header files

--- libsega3d/build-files.mk
+++ libsega3d/build-files.mk
@@ -1,6 +1,7 @@
 # -*- mode: makefile -*-
 
-LIB_SRCS:= sega3d.c tlist.c matrix_stack.c
+LIB_SRCS:= sega3d.c tlist.c sort.c matrix_stack.c
 
 INSTALL_HEADER_FILES:= \
-	./:sega3d.h:./sega3d/
+	./:sega3d.h:./sega3d/ \
+	./:sgl.h:./sega3d/
---
+++ libsega3d/sort.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+#include "sega3d.h"
+
+#include "sega3d-internal.h"
+
