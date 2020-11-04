fcdb703faf4ca07160a69043227a525e2af9089d
Israel Jacquez <mrkotfw@gmail.com>
2020-11-04 14:24:23 -0800

    Add 3-vector cross and cross magnitude functions

--- libyaul/math/fix16_vec3.c
+++ libyaul/math/fix16_vec3.c
@@ -25,7 +25,8 @@ fix16_vec3_normalize(fix16_vec3_t *result __unused)
 }
 
 void
-fix16_vec3_normalized(const fix16_vec3_t * __restrict v0, fix16_vec3_t * __restrict result)
+fix16_vec3_normalized(const fix16_vec3_t * __restrict v0,
+    fix16_vec3_t * __restrict result)
 {
         const fix16_t length = fix16_vec3_length(v0);
 
@@ -57,6 +58,32 @@ fix16_t
 fix16_vec3_dot(const fix16_vec3_t *a, const fix16_vec3_t *b)
 {
         return fix16_vec3_inline_dot(a, b);
+}
+
+void
+fix16_vec3_cross(const fix16_vec3_t * __restrict v0,
+    const fix16_vec3_t * __restrict v1, fix16_vec3_t * __restrict result)
+{
+        result->x = fix16_mul(v0->y, v1->z) - fix16_mul(v0->z, v1->y);
+        result->y = fix16_mul(v0->z, v1->x) - fix16_mul(v0->x, v1->z);
+        result->z = fix16_mul(v0->x, v1->y) - fix16_mul(v0->y, v1->x);
+}
+
+fix16_t
+fix16_vec3_cross_mag(const fix16_vec3_t * __restrict v0,
+    const fix16_vec3_t * __restrict v1)
+{
+        const fix16_t t1 = fix16_mul(v0->x, v1->y) - fix16_mul(v0->y, v1->x);
+        const fix16_t t2 = fix16_mul(v0->y, v1->z) - fix16_mul(v0->z, v1->y);
+        const fix16_t t3 = fix16_mul(v0->z, v1->x) - fix16_mul(v0->x, v1->z);
+
+        const fix16_t t1_2 = fix16_mul(t1, t1);
+        const fix16_t t2_2 = fix16_mul(t2, t2);
+        const fix16_t t3_2 = fix16_mul(t3, t3);
+
+        const fix16_t result = t1_2 + t2_2 + t3_2;
+
+        return result;
 }
 
 void
