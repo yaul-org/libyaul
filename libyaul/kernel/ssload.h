7ac5781f0d8f60ab205b3eabbc0282d227d85ea2
Israel Jacquez <mrkotfw@gmail.com>
2020-11-12 01:07:46 -0800

    Fix dynamic frame-rate bugs
    This was a huge change. It turns out that multiple requests for VDP2
    commits were being made. This caused major instability.
    
    There is also the change to using polling instead of interrupts for the
    DMA queue.

--- libyaul/kernel/ssload.h
+++ libyaul/kernel/ssload.h
@@ -8,10 +8,10 @@
 #ifndef _KERNEL_SSLOAD_H_
 #define _KERNEL_SSLOAD_H_
 
-#define SSLOAD_API_CMD_FILE     (0x00)
-#define SSLOAD_API_CMD_SIZE     (0x01)
-#define SSLOAD_API_CMD_LOG      (0x02)
-#define SSLOAD_API_CMD_QUIT     (0x7F)
+#define SSLOAD_API_CMD_FILE     (0xFB)
+#define SSLOAD_API_CMD_SIZE     (0xFC)
+#define SSLOAD_API_CMD_LOG      (0xFD)
+#define SSLOAD_API_CMD_QUIT     (0xFE)
 #define SSLOAD_API_CMD_INVALID  (0xFF)
 
 #endif /* !_KERNEL_SSLOAD_H_ */
