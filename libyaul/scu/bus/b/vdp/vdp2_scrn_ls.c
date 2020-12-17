f21491cd934e26672a87b26e3d1dca9df53fe9d4
Israel Jacquez <mrkotfw@gmail.com>
2020-12-16 21:18:54 -0800

    Expand line scroll support

--- libyaul/scu/bus/b/vdp/vdp2_scrn_ls.c
+++ libyaul/scu/bus/b/vdp/vdp2_scrn_ls.c
@@ -19,23 +19,24 @@ vdp2_scrn_ls_set(const vdp2_scrn_ls_format_t *ls)
                (ls->scroll_screen == VDP2_SCRN_NBG1));
 #endif /* DEBUG */
 
-        uint16_t lstau;
-        uint16_t lstal;
+        const uint16_t lstau = VDP2_VRAM_BANK(ls->line_scroll_table);
+        const uint16_t lstal = (ls->line_scroll_table >> 1) & 0xFFFF;
 
-        lstau = VDP2_VRAM_BANK(ls->line_scroll_table);
-        lstal = (ls->line_scroll_table >> 1) & 0xFFFF;
+        const uint16_t interval_bit = uint32_log2(ls->interval & 0x0F);
 
         switch (ls->scroll_screen) {
         case VDP2_SCRN_NBG0:
-                _state_vdp2()->regs->scrctl &= 0x3F01;
-                _state_vdp2()->regs->scrctl |= ls->enable;
+                _state_vdp2()->regs->scrctl &= 0xFFC1;
+                _state_vdp2()->regs->scrctl |= ls->enable << 1;
+                _state_vdp2()->regs->scrctl |= interval_bit << 4;
 
                 _state_vdp2()->regs->lsta0u = lstau;
                 _state_vdp2()->regs->lsta0l = lstal;
                 break;
         case VDP2_SCRN_NBG1:
-                _state_vdp2()->regs->scrctl &= 0x3F01;
-                _state_vdp2()->regs->scrctl |= ls->enable;
+                _state_vdp2()->regs->scrctl &= 0xC1FF;
+                _state_vdp2()->regs->scrctl |= ls->enable << 9;
+                _state_vdp2()->regs->scrctl |= interval_bit << 12;
 
                 _state_vdp2()->regs->lsta1u = lstau;
                 _state_vdp2()->regs->lsta1l = lstal;
