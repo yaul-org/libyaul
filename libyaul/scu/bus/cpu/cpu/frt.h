8f19c0c0c8ee7c5123ca067c386c92ef3272c6e4
Israel Jacquez <mrkotfw@gmail.com>
2021-02-09 17:06:03 -0800

    Document cpu/frt.h

--- libyaul/scu/bus/cpu/cpu/frt.h
+++ libyaul/scu/bus/cpu/cpu/frt.h
@@ -16,57 +16,77 @@
 
 __BEGIN_DECLS
 
-/// @defgroup CPU_FRT_HELPERS
-/// @defgroup CPU_FRT_FUNCTIONS
+/// @defgroup CPU_FRT_DEFINES CPU FRT defines
+/// @defgroup CPU_FRT_HELPERS CPU FRT helpers
+/// @defgroup CPU_FRT_INLINE_FUNCTIONS CPU FRT inline functions
+/// @defgroup CPU_FRT_FUNCTIONS CPU FRT functions
 
 /// @addtogroup CPU_FRT_DEFINES
 /// @{
 
-/// Not yet documented.
+/// @deprecated To be removed and replaced as an `enum`.
 #define CPU_FRT_CLOCK_DIV_8             0x00
-/// Not yet documented.
+/// @deprecated To be removed and replaced as an `enum`.
 #define CPU_FRT_CLOCK_DIV_32            0x01
-/// Not yet documented.
+/// @deprecated To be removed and replaced as an `enum`.
 #define CPU_FRT_CLOCK_DIV_128           0x02
 /// Not yet documented.
 #define CPU_FRT_CLOCK_DIV_RISING_EDGE   0x03
 
-/// Not yet documented.
+/// Tick count value representing 1ms for a NTSC machine in 320-mode with a ɸ/8
+/// clock divisor.
 #define CPU_FRT_NTSC_320_8_COUNT_1MS    0x0D1F
-/// Not yet documented.
+/// Tick count value representing 1ms for a NTSC machine in 320-mode with a ɸ/32
+/// clock divisor.
 #define CPU_FRT_NTSC_320_32_COUNT_1MS   0x0348
-/// Not yet documented.
+/// Tick count value representing 1ms for a NTSC machine in 320-mode with a
+/// ɸ/128 clock divisor.
 #define CPU_FRT_NTSC_320_128_COUNT_1MS  0x00D2
 
-/// Not yet documented.
+/// Tick count value representing 1ms for a NTSC machine in 352-mode with a ɸ/8
+/// clock divisor.
 #define CPU_FRT_NTSC_352_8_COUNT_1MS    0x0DFC
-/// Not yet documented.
+/// Tick count value representing 1ms for a NTSC machine in 352-mode with a ɸ/32
+/// clock divisor.
 #define CPU_FRT_NTSC_352_32_COUNT_1MS   0x037F
-/// Not yet documented.
+/// Tick count value representing 1ms for a NTSC machine in 352-mode with a
+/// ɸ/128 clock divisor.
 #define CPU_FRT_NTSC_352_128_COUNT_1MS  0x00E0
 
-/// Not yet documented.
+/// Tick count value representing 1ms for a PAL machine in 320-mode with a ɸ/8
+/// clock divisor.
 #define CPU_FRT_PAL_320_8_COUNT_1MS     0x0D08
-/// Not yet documented.
+/// Tick count value representing 1ms for a PAL machine in 320-mode with a ɸ/32
+/// clock divisor.
 #define CPU_FRT_PAL_320_32_COUNT_1MS    0x0342
-/// Not yet documented.
+/// Tick count value representing 1ms for a PAL machine in 320-mode with a ɸ/128
+/// clock divisor.
 #define CPU_FRT_PAL_320_128_COUNT_1MS   0x00D0
 
-/// Not yet documented.
+/// Tick count value representing 1ms for a PAL machine in 352-mode with a ɸ/8
+/// clock divisor.
 #define CPU_FRT_PAL_352_8_COUNT_1MS     0x0DFC
-/// Not yet documented.
+/// Tick count value representing 1ms for a PAL machine in 352-mode with a ɸ/32
+/// clock divisor.
 #define CPU_FRT_PAL_352_32_COUNT_1MS    0x037F
-/// Not yet documented.
+/// Tick count value representing 1ms for a PAL machine in 352-mode with a ɸ/128
+/// clock divisor.
 #define CPU_FRT_PAL_352_128_COUNT_1MS   0x00E0
 
 /// @}
 
+/// @brief Callback type.
+/// @see cpu_frt_oca_set
+/// @see cpu_frt_ocb_set
+/// @see cpu_frt_ovi_set
 typedef void (*cpu_frt_ihr)(void);
 
-/// @{
 /// @addtogroup CPU_FRT_INLINE_FUNCTIONS
+/// @{
 
-/// @brief Not yet documented.
+/// @brief Set the 2-byte FRT tick count.
+///
+/// @param count The 2-byte tick value to set.
 static inline void __always_inline
 cpu_frt_count_set(uint16_t count)
 {
@@ -74,7 +94,8 @@ cpu_frt_count_set(uint16_t count)
         MEMORY_WRITE(8, CPU(FRCL), (uint8_t)(count & 0xFF));
 }
 
-/// @brief Not yet documented.
+/// @brief Obtain the current 2-byte FRT tick count.
+/// @returns Return the 2-byte FRT tick count.
 static inline uint16_t __always_inline
 cpu_frt_count_get(void)
 {
@@ -87,7 +108,12 @@ cpu_frt_count_get(void)
         return (reg_frth << 8) | reg_frtl;
 }
 
-/// @brief Not yet documented.
+/// @brief Obtain the ICR value.
+///
+/// @details When a rising edge or falling edge of the input capture signal is
+/// detected, the current FRT tick count value is transferred to the ICR.
+///
+/// @returns The ICR value.
 static inline uint16_t __always_inline
 cpu_frt_input_capture_get(void)
 {
@@ -100,7 +126,8 @@ cpu_frt_input_capture_get(void)
         return (reg_ficrh << 8) | reg_ficrl;
 }
 
-/// @brief Not yet documented.
+/// @brief Obtain the interrupt priority level for CPU-FRT.
+/// @returns The interrupt priority level ranging from `0` to `15`.
 static inline uint8_t __always_inline
 cpu_frt_interrupt_priority_get(void)
 {
@@ -110,7 +137,9 @@ cpu_frt_interrupt_priority_get(void)
         return ((iprb >> 8) & 0x0F);
 }
 
-/// @brief Not yet documented.
+/// @brief Set the interrupt priority level for CPU-FRT.
+///
+/// @param priority The priority ranging from `0` to `15`.
 static inline void __always_inline
 cpu_frt_interrupt_priority_set(uint8_t priority)
 {
@@ -125,18 +154,24 @@ cpu_frt_interrupt_priority_set(uint8_t priority)
 /// @addtogroup CPU_FRT_HELPERS
 /// @{
 
-/// @brief Not yet documented.
-#define cpu_frt_oca_clear() do {                                               \
+/// @brief Clear the interrupt handler for the CPU-FRT OCA interrupt.
+/// @see cpu_frt_oca_set
+#define cpu_frt_oca_clear()                                                    \
+do {                                                                           \
         cpu_frt_oca_set(0, NULL);                                              \
 } while (false)
 
-/// @brief Not yet documented.
-#define cpu_frt_ocb_clear() do {                                               \
+/// @brief Clear the interrupt handler for the CPU-FRT OCB interrupt.
+/// @see cpu_frt_ocb_set
+#define cpu_frt_ocb_clear()                                                    \
+do {                                                                           \
         cpu_frt_ocb_set(0, NULL);                                              \
 } while (false)
 
-/// @brief Not yet documented.
-#define cpu_frt_ovi_clear() do {                                               \
+/// @brief Clear the interrupt handler for the CPU-FRT OVI interrupt.
+/// @see cpu_frt_ovi_set
+#define cpu_frt_ovi_clear()                                                    \
+do {                                                                           \
         cpu_frt_ovi_set(NULL);                                                 \
 } while (false)
 
@@ -145,14 +180,46 @@ cpu_frt_interrupt_priority_set(uint8_t priority)
 /// @addtogroup CPU_FRT_FUNCTIONS
 /// @{
 
-/// @brief Not yet documented.
-extern void cpu_frt_init(uint8_t);
-/// @brief Not yet documented.
-extern void cpu_frt_oca_set(uint16_t, cpu_frt_ihr);
-/// @brief Not yet documented.
-extern void cpu_frt_ocb_set(uint16_t, cpu_frt_ihr);
-/// @brief Not yet documented.
-extern void cpu_frt_ovi_set(cpu_frt_ihr);
+/// @brief Fully initialize the CPU-FRT depending with a specific clock divisor.
+///
+/// @details This function is heavy handed in that it resets the interrupt
+/// priority levels for all FRT related interrupts, clears the interrupt
+/// handlers, and resets the FRT count.
+///
+/// This function will properly initialize the CPU-FRT on either the master or
+/// slave CPU.
+///
+/// @param clock_div The clock divisor.
+extern void cpu_frt_init(uint8_t clock_div);
+
+/// @brief Set the interrupt handler for the CPU-FRT OCA interrupt.
+///
+/// @details There is no need to explicitly return via `rte` for @p ihr.
+///
+/// @param ihr   The interrupt handler
+/// @param count The tick count to trigger OCA on
+///
+/// @see cpu_frt_oca_clear
+extern void cpu_frt_oca_set(uint16_t count, cpu_frt_ihr ihr);
+
+/// @brief Set the interrupt handler for the CPU-FRT OCB interrupt.
+///
+/// @details There is no need to explicitly return via `rte` for @p ihr.
+///
+/// @param ihr   The interrupt handler
+/// @param count The tick count to trigger OCB on
+///
+/// @see cpu_frt_ocb_clear
+extern void cpu_frt_ocb_set(uint16_t count, cpu_frt_ihr ihr);
+
+/// @brief Set the interrupt handler for the CPU-FRT OVI interrupt.
+///
+/// @details There is no need to explicitly return via `rte` for @p ihr.
+///
+/// @param ihr The interrupt handler
+///
+/// @see cpu_frt_ovi_clear
+extern void cpu_frt_ovi_set(cpu_frt_ihr ihr);
 
 /// @}
 
