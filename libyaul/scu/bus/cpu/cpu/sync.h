7b43f735b34708ea48e009165e938272e2ecc0c0
Israel Jacquez <mrkotfw@gmail.com>
2021-02-10 13:10:10 -0800

    Document cpu/sync.h

--- libyaul/scu/bus/cpu/cpu/sync.h
+++ libyaul/scu/bus/cpu/cpu/sync.h
@@ -15,14 +15,27 @@
 __BEGIN_DECLS
 
 /// @defgroup CPU_SYNC CPU Synchronization
-/// Not yet documented.
+///
+/// @details All functions, otherwise noted make use of an exclusive area in
+/// HWRAM for all the lock variables. The size of the "lock array" is 256 bytes,
+/// and each lock variable is 1-byte.
 
 /// @addtogroup CPU_SYNC
 /// @{
 
-/// @brief Not yet documented.
+/// @brief Represent a lock in the BIOS lock array.
+typedef uint8_t cpu_sync_lock_t;
+
+/// @brief Provide functionality of mutual exclusion.
+///
+/// @details Implementation of the mutex is using test-and-set CPU instruction
+/// `tas.b`.
+/// 
+/// @param b The lock number in the lock array.
+///
+/// @returns `true` if locking was previously unlocked, otherwise `false`.
 static inline bool __always_inline
-cpu_sync_mutex(uint8_t b)
+cpu_sync_mutex(cpu_sync_lock_t b)
 {
         register uint8_t *bios_address;
         bios_address = (uint8_t *)HWRAM_UNCACHED(0x00000B00);
@@ -47,16 +60,25 @@ cpu_sync_mutex(uint8_t b)
         return result;
 }
 
-/// @brief Not yet documented.
+/// @brief Clear the lock.
+///
+/// @warning The clearing of @p b is not atomic.
+///
+/// @param b The lock number in the lock array.
 static inline void __always_inline
-cpu_sync_mutex_clear(uint8_t b)
+cpu_sync_mutex_clear(cpu_sync_lock_t b)
 {
         MEMORY_WRITE(8, HWRAM_UNCACHED(0x00000B00 + b), 0x00);
 }
 
-/// @brief Not yet documented.
+/// @brief Provide functionality of a spinlock.
+///
+/// @details Busy wait in a loop while repeatedly checking if the @p b is
+/// available.
+///
+/// @param b The lock number in the lock array.
 static inline void __always_inline
-cpu_sync_spinlock(uint8_t b)
+cpu_sync_spinlock(cpu_sync_lock_t b)
 {
         register uint8_t *bios_address;
         bios_address = (uint8_t *)HWRAM_UNCACHED(0x00000B00);
@@ -72,9 +94,13 @@ cpu_sync_spinlock(uint8_t b)
               [b] "r" (b));
 }
 
-/// @brief Not yet documented.
+/// @brief Clear the lock.
+///
+/// @warning The clearing of @p b is not atomic.
+///
+/// @param b The lock number in the lock array.
 static inline void __always_inline
-cpu_sync_spinlock_clear(uint8_t b)
+cpu_sync_spinlock_clear(cpu_sync_lock_t b)
 {
         cpu_sync_mutex_clear(b);
 }
