f1afb803e2dc5225fdb707be9d361f7e6c9cdb9a
Israel Jacquez <mrkotfw@gmail.com>
2021-02-10 12:18:49 -0800

    Document cpu/intc.h

--- libyaul/scu/bus/cpu/cpu/intc.h
+++ libyaul/scu/bus/cpu/cpu/intc.h
@@ -16,122 +16,120 @@
 __BEGIN_DECLS
 
 /// @defgroup CPU_INTC CPU Interrupt Controller (INTC)
-/// Not yet documented.
 
 /// @addtogroup CPU_INTC
 /// @{
 
-/// Not yet documented.
+/// Interrupt vector offset for master CPU.
 #define CPU_INTC_INTERRUPT_MASTER_BASE  0x0000
-/// Not yet documented.
+/// Interrupt vector offset for slave CPU.
 #define CPU_INTC_INTERRUPT_SLAVE_BASE   0x0100
 
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_POWER_ON_RESET_PC    0x00
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_POWER_ON_RESET_SP    0x01
-
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_MANUAL_RESET_PC      0x02
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_MANUAL_RESET_SP      0x03
-
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_ILLEGAL_INSTRUCTION  0x04
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_ILLEGAL_SLOT         0x06
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_CPU_ADDRESS_ERROR    0x09
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_DMA_ADDRESS_ERROR    0x0A
-
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_NMI                  0x0B
-
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_UBC                  0x0C
-
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_BREAK                0x20
-
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_SCI_ERI      0x60
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_SCI_RXI      0x61
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_SCI_TXI      0x62
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_SCI_TEI      0x63
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_FRT_ICI      0x64
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_FRT_OCI      0x65
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_FRT_OVI      0x66
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_FREE_67      0x67
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_WDT_ITI      0x68
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_BSC          0x69
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_FREE_6A      0x6A
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_FREE_6B      0x6B
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_DMAC0        0x6C
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_DMAC1        0x6D
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_DIVU_OVFI    0x6E
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_FREE_6F      0x6F
-/// @deprecated To be removed and replaced as an `enum`.
-#define CPU_INTC_INTERRUPT_SLAVE_ENTRY  0x94
-
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_VBLANK_IN             15
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_VBLANK_OUT            14
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_HBLANK_IN             13
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_TIMER_0               12
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_TIMER_1               11
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_DSP_END               10
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_SOUND_REQUEST         9
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_SYSTEM_MANAGER        8
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_PAD_INTERRUPT         8
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_LEVEL_2_DMA           6
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_LEVEL_1_DMA           6
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_LEVEL_0_DMA           5
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_DMA_ILLEGAL           3
-/// Not yet documented.
+/// Interrupts belonging to this priority.
 #define CPU_INTC_PRIORITY_SPRITE_END            2
+
+/// @brief Interrupt vectors.
+typedef enum cpu_intc_interrupt {
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_POWER_ON_RESET_PC   = 0x00,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_POWER_ON_RESET_SP   = 0x01,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_MANUAL_RESET_PC     = 0x02,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_MANUAL_RESET_SP     = 0x03,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_ILLEGAL_INSTRUCTION = 0x04,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_ILLEGAL_SLOT        = 0x06,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_CPU_ADDRESS_ERROR   = 0x09,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_DMA_ADDRESS_ERROR   = 0x0A,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_NMI                 = 0x0B,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_UBC                 = 0x0C,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_BREAK               = 0x20,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_SCI_ERI             = 0x60,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_SCI_RXI             = 0x61,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_SCI_TXI             = 0x62,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_SCI_TEI             = 0x63,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_FRT_ICI             = 0x64,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_FRT_OCI             = 0x65,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_FRT_OVI             = 0x66,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_FREE_67             = 0x67,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_WDT_ITI             = 0x68,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_BSC                 = 0x69,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_FREE_6A             = 0x6A,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_FREE_6B             = 0x6B,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_DMAC0               = 0x6C,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_DMAC1               = 0x6D,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_DIVU_OVFI           = 0x6E,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_FREE_6F             = 0x6F,
+        /// Interrupt vector.
+        CPU_INTC_INTERRUPT_SLAVE_ENTRY         = 0x94,
+} cpu_intc_interrupt_t;
 
 /// @brief Callback type.
 /// @see cpu_intc_ihr_set
 typedef void (*cpu_intc_ihr)(void);
 
-/// @brief Set the interrupt handler for the specified interrupt.
+/// @brief Set the interrupt handler for the specified CPU related interrupt.
 ///
-/// @details This is a BIOS call.
+/// @details This is a BIOS call. The function must use `rte` to return. Use the
+/// @p __interrupt_handler GCC attribute.
 ///
-/// There is no need to explicitly return via `rte` for @p ihr.
-///
+/// To set a handler in the slave CPU vector table, add the offset @ref
+/// CPU_INTC_INTERRUPT_SLAVE_BASE to @p vector.
+/// 
 /// @param vector The vector number.
 /// @param ihr    The interrupt handler.
 static inline void __always_inline
-cpu_intc_ihr_set(uint32_t vector, cpu_intc_ihr ihr)
+cpu_intc_ihr_set(cpu_intc_interrupt_t vector, cpu_intc_ihr ihr)
 {
         register uint32_t *bios_address;
         bios_address = (uint32_t *)0x06000310;
@@ -139,24 +137,30 @@ cpu_intc_ihr_set(uint32_t vector, cpu_intc_ihr ihr)
         ((void (*)(uint32_t, void (*)(void)))*bios_address)(vector, ihr);
 }
 
-/// @brief Clear the interrupt handler for the specified interrupt.
+/// @brief Clear the interrupt handler for the specified CPU related interrupt.
 ///
 /// @details This is a BIOS call.
 ///
+/// To clear a handler in the slave CPU vector table, add the offset @ref
+/// CPU_INTC_INTERRUPT_SLAVE_BASE to @p vector.
+///
 /// @param vector The vector number.
 static inline void __always_inline
-cpu_intc_ihr_clear(uint32_t vector)
+cpu_intc_ihr_clear(cpu_intc_interrupt_t vector)
 {
         cpu_intc_ihr_set(vector, NULL);
 }
 
-/// @brief Obtain the interrupt handler for the specified interrupt.
+/// @brief Obtain the interrupt handler for the specified CPU related interrupt.
 ///
 /// @details This is a BIOS call.
 ///
+/// To get a handler in the slave CPU vector table, add the offset @ref
+/// CPU_INTC_INTERRUPT_SLAVE_BASE to @p vector.
+/// 
 /// @param vector The vector number.
 static inline cpu_intc_ihr __always_inline
-cpu_intc_ihr_get(uint32_t vector)
+cpu_intc_ihr_get(cpu_intc_interrupt_t vector)
 {
         register uint32_t *bios_address;
         bios_address = (uint32_t *)0x06000314;
