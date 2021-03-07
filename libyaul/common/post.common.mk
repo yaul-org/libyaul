fd395e4c4c4241f2ab3c48f49271049832267813
Israel Jacquez <mrkotfw@gmail.com>
2021-03-07 11:18:05 -0800

    Implement writing output files to SH_BUILD_PATH

--- libyaul/common/post.common.mk
+++ libyaul/common/post.common.mk
@@ -4,25 +4,50 @@ ifeq ($(strip $(SH_PROGRAM)),)
   $(error Empty SH_PROGRAM (SH program name))
 endif
 
-# Check that SH_OBJECTS doesn't include duplicates
-# Be mindful that sort remove duplicates
-SH_OBJECTS_UNIQ= $(sort $(SH_OBJECTS))
-SH_OBJECTS_NO_LINK_UNIQ= $(sort $(SH_OBJECTS_NO_LINK))
+# Each relative or absolute path will be converted to using '@' instead of '/'
+# or '\\' (for Windows)
+define macro-convert-build-path
+  $(SH_BUILD_PATH)/$(subst /,@,$(abspath $1))
+endef
 
-ifeq ($(strip $(SH_OBJECTS_UNIQ)),)
-  # If both SH_OBJECTS_UNIQ and SH_OBJECTS_NO_LINK_UNIQ is empty
-  ifeq ($(strip $(SH_OBJECTS_NO_LINK_UNIQ)),)
-    $(error Empty SH_OBJECTS (SH object list))
+# Check that SH_SRCS and SH_SRCS_NO_LINK don't include duplicates. Be
+# mindful that sort remove duplicates
+SH_SRCS_UNIQ= $(sort $(SH_SRCS))
+SH_SRCS_NO_LINK_UNIQ= $(sort $(SH_SRCS_NO_LINK))
+
+SH_SRCS_C:= $(filter %.c,$(SH_SRCS_UNIQ))
+SH_SRCS_CXX:= $(filter %.cxx,$(SH_SRCS_UNIQ)) \
+	$(filter %.cpp,$(SH_SRCS_UNIQ)) \
+	$(filter %.cc,$(SH_SRCS_UNIQ)) \
+	$(filter %.C,$(SH_SRCS_UNIQ))
+SH_SRCS_S:= $(filter %.sx,$(SH_SRCS_UNIQ))
+SH_SRCS_ROMDISK:= $(filter %.romdisk,$(SH_SRCS_UNIQ))
+
+SH_SRCS_NO_LINK_C:= $(filter %.c,$(SH_SRCS_NO_LINK_UNIQ))
+SH_SRCS_NO_LINK_ROMDISK:= $(filter %.romdisk,$(SH_SRCS_NO_LINK_UNIQ))
+
+SH_OBJS_UNIQ:= $(addsuffix .o,$(foreach SRC,$(SH_SRCS_C) $(SH_SRCS_CXX) $(SH_SRCS_S),$(basename $(SRC)))) \
+	$(addsuffix .o,$(SH_SRCS_ROMDISK))
+SH_OBJS_UNIQ:= $(foreach OBJ,$(SH_OBJS_UNIQ),$(call macro-convert-build-path,$(OBJ)))
+
+SH_OBJS_NO_LINK_UNIQ:= $(addsuffix .o,$(foreach dir,$(SH_SRCS_NO_LINK_C),$(basename $(dir)))) \
+	$(addsuffix .o,$(SH_SRCS_NO_LINK_ROMDISK))
+SH_OBJS_NO_LINK_UNIQ:= $(foreach OBJ,$(SH_OBJS_NO_LINK_UNIQ),$(call macro-convert-build-path,$(OBJ)))
+
+ifeq ($(strip $(SH_OBJS_UNIQ)),)
+  # If both SH_OBJS_UNIQ and SH_OBJS_NO_LINK_UNIQ is empty
+  ifeq ($(strip $(SH_OBJS_NO_LINK_UNIQ)),)
+    $(error Empty SH_OBJS (SH source list))
   endif
 endif
 
-# Check that M68K_OBJECTS doesn't include duplicates
-M68K_OBJECTS_UNIQ= $(sort $(M68K_OBJECTS))
+# Check that M68K_OBJS doesn't include duplicates
+M68K_OBJS_UNIQ= $(sort $(M68K_OBJS))
 
 ifneq ($(strip $(M68K_PROGRAM)),)
   ifneq ($(strip $(M68K_PROGRAM)),undefined-program)
-    ifeq ($(strip $(M68K_OBJECTS_UNIQ)),)
-      $(error Empty M68K_OBJECTS (M68K object list))
+    ifeq ($(strip $(M68K_OBJS_UNIQ)),)
+      $(error Empty M68K_OBJS (M68K source list))
     endif
   endif
 endif
@@ -48,22 +73,21 @@ else
   SH_SPECS+= $(SH_CUSTOM_SPECS)
 endif
 
-ROMDISK_DEPS:= $(shell find ./romdisk -type f 2> /dev/null) $(ROMDISK_DEPS)
-
 ifneq ($(strip $(M68K_PROGRAM)),)
   ifneq ($(strip $(M68K_PROGRAM)),undefined-program)
-    # Check if there is a romdisk.o. If not, append to list of SH
-    # objects
-    ifeq ($(strip $(filter %.romdisk.o,$(SH_OBJECTS_UNIQ))),)
-      SH_OBJECTS_UNIQ+= root.romdisk.o
+    # Check if there is a romdisk.o and append the object file to the end of the
+    # list of SH objects
+	# XXX: Change this... check SH_SRCS_ROMDISK and do a foreach on each romdisk
+	ifeq ($(strip $(filter %.romdisk,$(SH_SRCS_UNIQ))),)
+      SH_OBJS_NO_LINK_UNIQ+= root.romdisk.o
     endif
-    ROMDISK_DEPS+= ./romdisk/$(M68K_PROGRAM).m68k
+    # ROMDISK_DEPS+= ./romdisk/$(M68K_PROGRAM).m68k
   endif
 endif
 
-SH_DEPS:= $(SH_OBJECTS_UNIQ:.o=.d)
-SH_TEMPS:= $(SH_OBJECTS_UNIQ:.o=.i) $(SH_OBJECTS_UNIQ:.o=.ii) $(SH_OBJECTS_UNIQ:.o=.s)
-SH_DEPS_NO_LINK:= $(SH_OBJECTS_NO_LINK_UNIQ:.o=.d)
+SH_DEPS:= $(SH_OBJS_UNIQ:.o=.d)
+SH_TEMPS:= $(SH_OBJS_UNIQ:.o=.i) $(SH_OBJS_UNIQ:.o=.ii) $(SH_OBJS_UNIQ:.o=.s)
+SH_DEPS_NO_LINK:= $(SH_OBJS_NO_LINK_UNIQ:.o=.d)
 
 # Parse out included paths from GCC when the specs files are used. This is used
 # to explictly populate each command database entry with include paths
@@ -87,22 +111,77 @@ define macro-update-cdb
 endef
 endif
 
+# $1 -> $<
+# $2 -> $@
+define macro-generate-sh-build-object
+$2: $1
+	@printf -- "$(V_BEGIN_YELLOW)$1$(V_END)\n"
+	$(ECHO)mkdir -p $(SH_BUILD_PATH)
+	$(ECHO)$(SH_CC) -MF $(addsuffix .d,$(basename $2)) -MD $(SH_CFLAGS) $(foreach specs,$(SH_SPECS),-specs=$(specs)) -c -o $2 $1
+	$(ECHO)$(call macro-update-cdb,\
+		$(CDB_GCC),\
+		$(abspath $1),\
+		$(abspath $2),\
+		$(dir $(abspath $1)),\
+		$(CDB_FILE),\
+		$(SH_CFLAGS) $(SH_SYSTEM_INCLUDE_DIRS) $(SH_INCLUDE_DIRS))
+endef
+
+# $1 -> $<
+# $2 -> $@
+define macro-generate-sh-build-asm-object
+$2: $1
+	@printf -- "$(V_BEGIN_YELLOW)$1$(V_END)\n"
+	$(ECHO)mkdir -p $(SH_BUILD_PATH)
+	$(ECHO)$(SH_AS) $(SH_AFLAGS) -o $2 $1
+endef
+
+# $1 -> $<
+# $2 -> $@
+define macro-generate-sh-build-c++-object
+$2: $1
+	@printf -- "$(V_BEGIN_YELLOW)$1$(V_END)\n"
+	$(ECHO)mkdir -p $(SH_BUILD_PATH)
+	$(ECHO)$(SH_CXX) -MF $(addsuffix .d,$(basename $2)) -MD $(SH_CXXFLAGS) $(foreach specs,$(SH_SPECS),-specs=$(specs)) -c -o $2 $1
+	$(ECHO)$(call macro-update-cdb,\
+		$(CDB_CPP),\
+		$(abspath $1),\
+		$(abspath $2),\
+		$(dir $(abspath $1)),\
+		$(CDB_FILE),\
+		$(SH_CXXFLAGS) $(SH_SYSTEM_INCLUDE_DIRS) $(SH_INCLUDE_DIRS))
+endef
+
+# $1 -> Source filename
+# $2 -> Absolute filename build path
+# $3 -> Directory
+define macro-generate-romdisk-rule
+$2:
+	@printf -- "$(V_BEGIN_YELLOW)$(strip $1)$(V_END)\n"
+	$(ECHO)mkdir -p $(SH_BUILD_PATH)
+	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/genromfs $(ROMDISK_FLAGS) -d $3 -f - > $2
+
+$2.o: $2
+	@printf -- "$(V_BEGIN_YELLOW)$(strip $1).o$(V_END)\n"
+	$(ECHO)$(YAUL_INSTALL_ROOT)/bin/bin2o $$< `echo "$(strip $1)" | sed -E 's/[\. ]/_/g'` $$@
+endef
+
 $(SH_PROGRAM): $(SH_PROGRAM).cue
 
 all: $(SH_PROGRAM).cue
 
 example: all
 
-$(SH_PROGRAM).bin: $(SH_PROGRAM).elf
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
+$(SH_BUILD_PATH)/$(SH_PROGRAM).bin: $(SH_BUILD_PATH)/$(SH_PROGRAM).elf
+	@printf -- "$(V_BEGIN_YELLOW)$(@F)$(V_END)\n"
 	$(ECHO)$(SH_OBJCOPY) -O binary $< $@
 	@[ -z "${SILENT}" ] && du -hs $@ | awk '{ print $$1 }' || true
 
-$(SH_PROGRAM).elf: $(SH_OBJECTS_UNIQ) $(SH_OBJECTS_NO_LINK_UNIQ)
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(SH_LD) $(foreach specs,$(SH_SPECS),-specs=$(specs)) $(SH_OBJECTS_UNIQ) $(SH_LDFLAGS) $(foreach lib,$(SH_LIBRARIES),-l$(lib)) -o $@
-	$(ECHO)$(SH_NM) $(SH_PROGRAM).elf > $(SH_PROGRAM).sym
-	$(ECHO)$(SH_OBJDUMP) -S $(SH_PROGRAM).elf > $(SH_PROGRAM).asm
+$(SH_BUILD_PATH)/$(SH_PROGRAM).elf: $(SH_OBJS_UNIQ) $(SH_OBJS_NO_LINK_UNIQ)
+	@printf -- "$(V_BEGIN_YELLOW)$(@F)$(V_END)\n"
+	$(ECHO)$(SH_LD) $(foreach specs,$(SH_SPECS),-specs=$(specs)) $(SH_OBJS_UNIQ) $(SH_LDFLAGS) $(foreach lib,$(SH_LIBRARIES),-l$(lib)) -o $@
+	$(ECHO)$(SH_NM) $(SH_BUILD_PATH)/$(SH_PROGRAM).elf > $(SH_BUILD_PATH)/$(SH_PROGRAM).sym
+	$(ECHO)$(SH_OBJDUMP) -S $(SH_BUILD_PATH)/$(SH_PROGRAM).elf > $(SH_BUILD_PATH)/$(SH_PROGRAM).asm
 
 ./romdisk/$(M68K_PROGRAM).m68k: $(M68K_PROGRAM).m68k.elf
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
@@ -110,109 +189,55 @@ $(SH_PROGRAM).elf: $(SH_OBJECTS_UNIQ) $(SH_OBJECTS_NO_LINK_UNIQ)
 	$(ECHO)chmod -x $@
 	@du -hs $@ | awk '{ print $$1 " ""'"($@)"'" }'
 
-$(M68K_PROGRAM).m68k.elf: $(M68K_OBJECTS_UNIQ)
+$(M68K_PROGRAM).m68k.elf: $(M68K_OBJS_UNIQ)
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(M68K_LD) $(M68K_OBJECTS_UNIQ) $(M68K_LDFLAGS) -o $@
+	$(ECHO)$(M68K_LD) $(M68K_OBJS_UNIQ) $(M68K_LDFLAGS) -o $@
 	$(ECHO)$(M68K_NM) $(M68K_PROGRAM).m68k.elf > $(M68K_PROGRAM).m68k.sym
 
-./romdisk:
-	$(ECHO)mkdir -p $@
+$(foreach SRC,$(SH_SRCS_C), \
+	$(eval $(call macro-generate-sh-build-object,$(SRC),\
+		$(call macro-convert-build-path,$(addsuffix .o,$(basename $(SRC)))))))
 
-%.romdisk: ./romdisk $(ROMDISK_DEPS)
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/genromfs $(ROMDISK_FLAGS) -d ./romdisk/ -f $@
+$(foreach SRC,$(SH_SRCS_CXX), \
+	$(eval $(call macro-generate-sh-build-c++-object,$(SRC),\
+		$(call macro-convert-build-path,$(addsuffix .o,$(basename $(SRC)))))))
 
-%.romdisk.o: %.romdisk
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(YAUL_INSTALL_ROOT)/bin/fsck.genromfs ./romdisk/
-	$(ECHO)$(YAUL_INSTALL_ROOT)/bin/bin2o $< `echo "$<" | sed -E 's/[\. ]/_/g'` $@
-	$(ECHO)$(RM) $<
+$(foreach SRC,$(SH_SRCS_S), \
+	$(eval $(call macro-generate-sh-build-asm-object,$(SRC),\
+		$(call macro-convert-build-path,$(addsuffix .o,$(basename $(SRC)))))))
 
-%.o: %.c
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(SH_CC) -MF $(abspath $*.d) -MD $(SH_CFLAGS) $(foreach specs,$(SH_SPECS),-specs=$(specs)) -c -o $@ $<
-	$(ECHO)$(call macro-update-cdb,\
-		$(CDB_GCC),\
-		$(abspath $(<)),\
-		$(abspath $(@)),\
-		$(abspath $(<D)),\
-		$(CDB_FILE),\
-		$(SH_CFLAGS) $(SH_SYSTEM_INCLUDE_DIRS) $(SH_INCLUDE_DIRS))
-
-%.o: %.cc
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(SH_CXX) -MF $(abspath $*.d) -MD $(SH_CXXFLAGS) $(foreach specs,$(SH_SPECS),-specs=$(specs)) -c -o $@ $<
-	$(ECHO)$(call macro-update-cdb,\
-		$(CDB_CPP),\
-		$(abspath $(<)),\
-		$(abspath $(@)),\
-		$(abspath $(<D)),\
-		$(CDB_FILE),\
-		$(SH_CXXFLAGS) $(SH_SYSTEM_INCLUDE_DIRS) $(SH_INCLUDE_DIRS))
-
-%.o: %.C
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(SH_CXX) -MF $(abspath $*.d) -MD $(SH_CXXFLAGS) $(foreach specs,$(SH_SPECS),-specs=$(specs)) -c -o $@ $<
-	$(ECHO)$(call macro-update-cdb,\
-		$(CDB_CPP),\
-		$(abspath $(<)),\
-		$(abspath $(@)),\
-		$(abspath $(<D)),\
-		$(CDB_FILE),\
-		$(SH_CXXFLAGS) $(SH_SYSTEM_INCLUDE_DIRS) $(SH_INCLUDE_DIRS))
-
-%.o: %.cpp
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(SH_CXX) -MF $(abspath $*.d) -MD $(SH_CXXFLAGS) $(foreach specs,$(SH_SPECS),-specs=$(specs)) -c -o $@ $<
-	$(ECHO)$(call macro-update-cdb,\
-		$(CDB_CPP),\
-		$(abspath $(<)),\
-		$(abspath $(@)),\
-		$(abspath $(<D)),\
-		$(CDB_FILE),\
-		$(SH_CXXFLAGS) $(SH_SYSTEM_INCLUDE_DIRS) $(SH_INCLUDE_DIRS))
-
-%.o: %.cxx
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(SH_CXX) -MF $(abspath $*.d) -MD $(SH_CXXFLAGS) $(foreach specs,$(SH_SPECS),-specs=$(specs)) -c -o $@ $<
-	$(ECHO)$(call macro-update-cdb,\
-		$(CDB_CPP),\
-		$(abspath $(<)),\
-		$(abspath $(@)),\
-		$(abspath $(<D)),\
-		$(CDB_FILE),\
-		$(SH_CXXFLAGS) $(SH_SYSTEM_INCLUDE_DIRS) $(SH_INCLUDE_DIRS))
-
-%.o: %.sx
-	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)$(SH_AS) $(SH_AFLAGS) -o $@ $<
+$(foreach SRC,$(SH_SRCS_ROMDISK), \
+	$(eval $(call macro-generate-romdisk-rule,\
+		$(SRC),\
+		$(call macro-convert-build-path,$(SRC)),\
+		./romdisk/)))
 
 %.m68k.o: %.m68k.sx
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
 	$(ECHO)$(M68K_AS) $(M68K_AFLAGS) -o $@ $<
 
-$(SH_PROGRAM).iso: $(SH_PROGRAM).bin IP.BIN $(shell find $(IMAGE_DIRECTORY)/ -type f 2>/dev/null)
+$(SH_PROGRAM).iso: $(SH_BUILD_PATH)/$(SH_PROGRAM).bin $(SH_BUILD_PATH)/IP.BIN
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
 	$(ECHO)mkdir -p $(IMAGE_DIRECTORY)
-	$(ECHO)cp $(SH_PROGRAM).bin $(IMAGE_DIRECTORY)/$(IMAGE_1ST_READ_BIN)
+	$(ECHO)cp $(SH_BUILD_PATH)/$(SH_PROGRAM).bin $(IMAGE_DIRECTORY)/$(IMAGE_1ST_READ_BIN)
 	$(ECHO)for txt in "ABS.TXT" "BIB.TXT" "CPY.TXT"; do \
 	    if ! [ -s $(IMAGE_DIRECTORY)/$$txt ]; then \
 		printf -- "empty\n" > $(IMAGE_DIRECTORY)/$$txt; \
 	    fi \
 	done
-	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/make-iso $(IMAGE_DIRECTORY) $(SH_PROGRAM)
+	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/make-iso $(IMAGE_DIRECTORY) $(SH_BUILD_PATH)/IP.BIN $(SH_PROGRAM)
 
-$(SH_PROGRAM).ss: $(SH_PROGRAM).bin CART-IP.BIN
+$(SH_PROGRAM).ss: $(SH_BUILD_PATH)/$(SH_PROGRAM).bin $(SH_BUILD_PATH)/CART-IP.BIN
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
-	$(ECHO)cat CART-IP.BIN $(SH_PROGRAM).bin > $@
+	$(ECHO)cat $(SH_BUILD_PATH)/CART-IP.BIN $(SH_BUILD_PATH)/$(SH_PROGRAM).bin > $@
 
 $(SH_PROGRAM).cue: $(SH_PROGRAM).iso
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
 	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/make-cue "$(SH_PROGRAM).iso"
 
-IP.BIN: $(YAUL_INSTALL_ROOT)/share/yaul/bootstrap/ip.sx $(SH_PROGRAM).bin
+$(SH_BUILD_PATH)/IP.BIN: $(YAUL_INSTALL_ROOT)/share/yaul/bootstrap/ip.sx $(SH_BUILD_PATH)/$(SH_PROGRAM).bin
 	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/make-ip \
-	    "$(SH_PROGRAM).bin" \
+	    "$(SH_BUILD_PATH)/$(SH_PROGRAM).bin" \
 		"$(IP_VERSION)" \
 		$(IP_RELEASE_DATE) \
 		"$(IP_AREAS)" \
@@ -223,9 +248,9 @@ IP.BIN: $(YAUL_INSTALL_ROOT)/share/yaul/bootstrap/ip.sx $(SH_PROGRAM).bin
 		$(IP_1ST_READ_ADDR) \
 	    $(IP_1ST_READ_SIZE)
 
-CART-IP.BIN: $(YAUL_INSTALL_ROOT)/share/yaul/bootstrap/ip.sx $(SH_PROGRAM).bin
+$(SH_BUILD_PATH)/CART-IP.BIN: $(YAUL_INSTALL_ROOT)/share/yaul/bootstrap/ip.sx $(SH_BUILD_PATH)/$(SH_PROGRAM).bin
 	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/make-ip \
-	    "$(SH_PROGRAM).bin" \
+	    "$(SH_BUILD_PATH)/$(SH_PROGRAM).bin" \
 		"$(IP_VERSION)" \
 		$(IP_RELEASE_DATE) \
 		"$(IP_AREAS)" \
@@ -234,31 +259,29 @@ CART-IP.BIN: $(YAUL_INSTALL_ROOT)/share/yaul/bootstrap/ip.sx $(SH_PROGRAM).bin
 		$(IP_MASTER_STACK_ADDR) \
 		$(IP_SLAVE_STACK_ADDR) \
 		$(IP_1ST_READ_ADDR) \
-	    -1 && mv -f IP.BIN CART-IP.BIN
+	    -1 && mv -f $(SH_BUILD_PATH)/IP.BIN $(SH_BUILD_PATH)/CART-IP.BIN
 
 clean:
 	$(ECHO)printf -- "$(V_BEGIN_CYAN)$(SH_PROGRAM)$(V_END) $(V_BEGIN_GREEN)clean$(V_END)\n"
 	$(ECHO)-rm -f \
-	    $(SH_PROGRAM).bin \
 	    $(SH_PROGRAM).cue \
 	    $(SH_PROGRAM).iso \
 	    $(SH_PROGRAM).ss \
-	    $(SH_OBJECTS_UNIQ) \
+	    $(SH_OBJS_UNIQ) \
 	    $(SH_DEPS) \
 	    $(SH_DEPS_NO_LINK) \
 	    $(SH_TEMPS) \
-	    $(SH_PROGRAM).asm \
-	    $(SH_PROGRAM).bin \
-	    $(SH_PROGRAM).elf \
-	    $(SH_PROGRAM).map \
-	    $(SH_PROGRAM).sym \
-	    $(SH_OBJECTS_NO_LINK_UNIQ) \
+	    $(SH_BUILD_PATH)/$(SH_PROGRAM).asm \
+	    $(SH_BUILD_PATH)/$(SH_PROGRAM).bin \
+	    $(SH_BUILD_PATH)/$(SH_PROGRAM).elf \
+	    $(SH_BUILD_PATH)/$(SH_PROGRAM).map \
+	    $(SH_BUILD_PATH)/$(SH_PROGRAM).sym \
+	    $(SH_OBJS_NO_LINK_UNIQ) \
 	    $(SH_DEPS_NO_LINK) \
-	    root.romdisk \
-	    IP.BIN \
-	    IP.BIN.map \
-		CART-IP.BIN \
-		CART-IP.BIN.map \
+	    $(SH_BUILD_PATH)/IP.BIN \
+	    $(SH_BUILD_PATH)/IP.BIN.map \
+		$(SH_BUILD_PATH)/CART-IP.BIN \
+		$(SH_BUILD_PATH)/CART-IP.BIN.map \
 	    $(CDB_FILE)
 ifneq ($(strip $(M68K_PROGRAM)),)
 	$(ECHO)printf -- "$(V_BEGIN_CYAN)$(M68K_PROGRAM)$(V_END) $(V_BEGIN_GREEN)clean$(V_END)\n"
@@ -266,7 +289,7 @@ ifneq ($(strip $(M68K_PROGRAM)),)
 	    romdisk/$(M68K_PROGRAM).m68k \
 	    $(M68K_PROGRAM).m68k.elf \
 	    $(M68K_PROGRAM).m68k.sym \
-	    $(M68K_OBJECTS)
+	    $(M68K_OBJS)
 endif
 
 list-targets:
