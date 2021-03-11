7dc4e805448b050dcefd995f299df203e63ce878
Israel Jacquez <mrkotfw@gmail.com>
2021-03-11 01:16:12 -0800

    Document arp/arp.h

--- libyaul/scu/bus/a/cs0/arp/arp/map.h
+++ libyaul/scu/bus/a/cs0/arp/arp/map.h
@@ -8,12 +8,41 @@
 #ifndef _ARP_MAP_H_
 #define _ARP_MAP_H_
 
-/* Macros specific for processor */
-#define ARP(x)  (0x22000000UL + (x))
+/// @addtogroup MEMORY_MAP_AREAS
+/// @{
 
+/// @brief Specify offset @p x for address space.
+///
+/// @param x The byte offset.
+#define ARP(x) (0x22000000UL + (x))
+
+/// @}
+
+/// @addtogroup MEMORY_MAP
+/// @defgroup MEMORY_MAP_ARP_IO_REGISTERS ARP I/O
+/// @ingroup MEMORY_MAP
+/// @{
+
+/// @brief ARP I/O register.
+/// @see MEMORY_WRITE
+/// @see MEMORY_READ
 #define OUTPUT  0x00080001UL
+
+/// @brief ARP I/O register.
+/// @see MEMORY_WRITE
+/// @see MEMORY_READ
 #define STATUS  0x00100001UL
+
+/// @brief ARP I/O register.
+/// @see MEMORY_WRITE
+/// @see MEMORY_READ
 #define INPUT   0x00180001UL
+
+/// @brief ARP I/O register.
+/// @see MEMORY_WRITE
+/// @see MEMORY_READ
 #define VERSION 0x00004AF0UL
+
+/// @}
 
 #endif /* !_ARP_MAP_H_ */
