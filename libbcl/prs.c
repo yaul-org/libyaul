c666faea8c65214b194b7e6bb2781d51bd328683
Israel Jacquez <mrkotfw@gmail.com>
2021-10-02 14:13:41 -0700

    Add bcl_prs tool

--- libbcl/prs.c
+++ libbcl/prs.c
@@ -1,3 +1,7 @@
+/* Archive of the original code from fuzziqer. No license was provided with the
+ * released files, although fuzziqer stated using the source freely was fine as
+ * long as he's credited for the compression and decompression code. */
+
 #include <sys/cdefs.h>
 
 #include <stdbool.h>
@@ -6,93 +10,107 @@
 void
 bcl_prs_decompress(void *in, void *out)
 {
-        uint32_t r3,r5; // 6 unnamed registers
-        uint32_t bitpos = 9; // 4 named registers
-        uint8_t* sourceptr = (uint8_t*) in;
-        uint8_t* destptr = (uint8_t*) out;
-        uint8_t currentbyte;
+        uint32_t r3, r5; /* 6 unnamed registers */
+        uint32_t bitpos = 9; /* 4 named registers */
+        uint8_t *src_ptr = (uint8_t *)in;
+        uint8_t *dst_ptr = (uint8_t *)out;
+        uint8_t current_byte;
         bool flag;
         int offset;
-        uint32_t x,t; // 2 placed variables
+        uint32_t x, t; /* 2 placed variables */
 
-        //printf("\n> decompressing\n");
-        currentbyte = sourceptr[0];
-        sourceptr++;
+        current_byte = src_ptr[0];
+        src_ptr++;
 
         while (true) {
                 bitpos--;
+
                 if (bitpos == 0) {
-                        currentbyte = sourceptr[0];
+                        current_byte = src_ptr[0];
                         bitpos = 8;
-                        sourceptr++;
+                        src_ptr++;
                 }
-                flag = currentbyte & 1;
-                currentbyte = currentbyte >> 1;
+
+                flag = current_byte & 1;
+                current_byte = current_byte >> 1;
+
                 if (flag) {
-                        destptr[0] = sourceptr[0];
-                        //printf("> > > %08X->%08X byte\n",sourceptr - sourceptr_orig,destptr - destptr_orig);
-                        sourceptr++;
-                        destptr++;
+                        dst_ptr[0] = src_ptr[0];
+                        src_ptr++;
+                        dst_ptr++;
                         continue;
                 }
+
                 bitpos--;
+
                 if (bitpos == 0) {
-                        currentbyte = sourceptr[0];
+                        current_byte = src_ptr[0];
                         bitpos = 8;
-                        sourceptr++;
+                        src_ptr++;
                 }
-                flag = currentbyte & 1;
-                currentbyte = currentbyte >> 1;
+
+                flag = current_byte & 1;
+                current_byte = current_byte >> 1;
+
                 if (flag) {
-                        r3 = sourceptr[0] & 0xFF;
-                        //printf("> > > > > first: %02X - ",sourceptr[0]); system("PAUSE");
-                        offset = ((sourceptr[1] & 0xFF) << 8) | r3;
-                        //printf("> > > > > second: %02X - ",sourceptr[1]); system("PAUSE");
-                        sourceptr += 2;
+                        r3 = src_ptr[0] & 0xFF;
+
+                        offset = ((src_ptr[1] & 0xFF) << 8) | r3;
+
+                        src_ptr += 2;
+
                         if (offset == 0) {
                                 return;
                         }
+
                         r3 = r3 & 0x00000007;
                         r5 = (offset >> 3) | 0xFFFFE000;
+
                         if (r3 == 0) {
                                 flag = 0;
-                                r3 = sourceptr[0] & 0xFF;
-                                sourceptr++;
+                                r3 = src_ptr[0] & 0xFF;
+                                src_ptr++;
                                 r3++;
                         } else {
                                 r3 += 2;
                         }
-                        r5 += (uint32_t) destptr;
-                        //printf("> > > %08X->%08X ldat %08X %08X %s\n",sourceptr - sourceptr_orig,destptr - destptr_orig,r5 - (uint32_t) destptr,r3,flag ? "inline" : "extended");
+
+                        r5 += (uint32_t) dst_ptr;
                 } else {
                         r3 = 0;
+
                         for (x = 0; x < 2; x++) {
                                 bitpos--;
+
                                 if (bitpos == 0) {
-                                        currentbyte = sourceptr[0];
+                                        current_byte = src_ptr[0];
                                         bitpos = 8;
-                                        sourceptr++;
+                                        src_ptr++;
                                 }
-                                flag = currentbyte & 1;
-                                currentbyte = currentbyte >> 1;
+
+                                flag = current_byte & 1;
+                                current_byte = current_byte >> 1;
                                 offset = r3 << 1;
                                 r3 = offset | flag;
                         }
-                        offset = sourceptr[0] | 0xFFFFFF00;
+
+                        offset = src_ptr[0] | 0xFFFFFF00;
                         r3 += 2;
-                        sourceptr++;
-                        r5 = offset + (uint32_t) destptr;
-                        //printf("> > > %08X->%08X sdat %08X %08X\n",sourceptr - sourceptr_orig,destptr - destptr_orig,r5 - (uint32_t) destptr,r3);
+                        src_ptr++;
+                        r5 = offset + (uint32_t)dst_ptr;
                 }
+
                 if (r3 == 0) {
                         continue;
                 }
+
                 t = r3;
+
                 for (x = 0; x < t; x++) {
-                        destptr[0] = * (uint8_t *)r5;
+                        dst_ptr[0] = * (uint8_t *)r5;
                         r5++;
                         r3++;
-                        destptr++;
+                        dst_ptr++;
                 }
         }
 }
