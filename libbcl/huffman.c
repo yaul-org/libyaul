24fd0c3078576e656a839b0dd176f9baf515a705
Israel Jacquez <mrkotfw@gmail.com>
2021-10-02 18:21:55 -0700

    Remove unused functions

--- libbcl/huffman.c
+++ libbcl/huffman.c
@@ -53,11 +53,8 @@ struct huff_decodenode_struct {
 };
 
 static void _bitstream_init(huff_bitstream_t *stream, uint8_t *buf);
-
 static uint32_t _bit_read(huff_bitstream_t *stream);
 static uint32_t _8bits_read(huff_bitstream_t *stream);
-static void _bits_write(huff_bitstream_t *stream, uint32_t x, uint32_t bits);
-static void _tree_store(huff_encodenode_t *node, huff_sym_t *sym, huff_bitstream_t *stream, uint32_t code, uint32_t bits);
 static huff_decodenode_t *_tree_recover(huff_decodenode_t *nodes, huff_bitstream_t *stream, uint32_t *nodenum);
 
 void
@@ -151,72 +148,6 @@ _8bits_read(huff_bitstream_t *stream)
         stream->BytePtr = buf;
 
         return x;
-}
-
-/* Write bits to a bitstream */
-static void
-_bits_write(huff_bitstream_t *stream, uint32_t x, uint32_t bits)
-{
-        uint32_t  bit, count;
-        uint8_t *buf;
-        uint32_t  mask;
-
-        /* Get current stream state */
-        buf = stream->BytePtr;
-        bit = stream->BitPos;
-
-        /* Append bits */
-        mask = 1 << (bits-1);
-        for (count = 0; count < bits; ++ count) {
-                *buf = (*buf & (0xff^ (1<< (7-bit)))) +
-                       ((x & mask ? 1 : 0) << (7-bit));
-                x <<= 1;
-                bit = (bit+1) & 7;
-                if (!bit) {
-                        ++ buf;
-                }
-        }
-
-        /* Store new stream state */
-        stream->BytePtr = buf;
-        stream->BitPos  = bit;
-}
-
-/* Store a Huffman tree in the output stream and in a look-up-table (a symbol
- * array) */
-static void
-_tree_store(huff_encodenode_t *node, huff_sym_t *sym,
-    huff_bitstream_t *stream, uint32_t code, uint32_t bits)
-{
-        uint32_t sym_idx;
-
-        /* Is this a leaf node? */
-        if (node->Symbol >= 0) {
-                /* Append symbol to tree description */
-                _bits_write(stream, 1, 1);
-                _bits_write(stream, node->Symbol, 8);
-
-                /* Find symbol index */
-                for (sym_idx = 0; sym_idx < 256; ++ sym_idx) {
-                        if (sym[sym_idx].Symbol == node->Symbol) {
-                                break;
-                        }
-                }
-
-                /* Store code info in symbol array */
-                sym[sym_idx].Code = code;
-                sym[sym_idx].Bits = bits;
-                return;
-        } else {
-                /* This was not a leaf node */
-                _bits_write(stream, 0, 1);
-        }
-
-        /* Branch A */
-        _tree_store(node->ChildA, sym, stream, (code<<1)+0, bits+1);
-
-        /* Branch B */
-        _tree_store(node->ChildB, sym, stream, (code<<1)+1, bits+1);
 }
 
 /* Recover a Huffman tree from a bitstream */
