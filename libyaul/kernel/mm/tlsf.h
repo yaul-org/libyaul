67cff41fa75b044aa8d62666286dca47a234605c
Israel Jacquez <mrkotfw@gmail.com>
2021-10-28 00:28:31 -0700

    Clean up coding style

--- libyaul/kernel/mm/tlsf.h
+++ libyaul/kernel/mm/tlsf.h
@@ -38,54 +38,52 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
+
 #include <stddef.h>
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
+__BEGIN_DECLS
 
 /* tlsf_t: a TLSF structure. Can contain 1 to N pools. */
 /* pool_t: a block of memory that TLSF can manage. */
-typedef void* tlsf_t;
-typedef void* pool_t;
+typedef void *tlsf_t;
+typedef void *pool_t;
 
 /* Create/destroy a memory pool. */
-tlsf_t tlsf_create(void* mem);
-tlsf_t tlsf_create_with_pool(void* mem, size_t bytes);
-void tlsf_destroy(tlsf_t tlsf);
-pool_t tlsf_get_pool(tlsf_t tlsf);
+extern tlsf_t tlsf_create(void *mem);
+extern tlsf_t tlsf_create_with_pool(void *mem, size_t bytes);
+extern void tlsf_destroy(tlsf_t tlsf);
+extern pool_t tlsf_get_pool(tlsf_t tlsf);
 
 /* Add/remove memory pools. */
-pool_t tlsf_add_pool(tlsf_t tlsf, void* mem, size_t bytes);
-void tlsf_remove_pool(tlsf_t tlsf, pool_t pool);
+extern pool_t tlsf_add_pool(tlsf_t tlsf, void *mem, size_t bytes);
+extern void tlsf_remove_pool(tlsf_t tlsf, pool_t pool);
 
 /* malloc/memalign/realloc/free replacements. */
-void* tlsf_malloc(tlsf_t tlsf, size_t bytes);
-void* tlsf_memalign(tlsf_t tlsf, size_t align, size_t bytes);
-void* tlsf_realloc(tlsf_t tlsf, void* ptr, size_t size);
-void tlsf_free(tlsf_t tlsf, void* ptr);
+extern void *tlsf_malloc(tlsf_t tlsf, size_t bytes);
+extern void *tlsf_memalign(tlsf_t tlsf, size_t align, size_t bytes);
+extern void *tlsf_realloc(tlsf_t tlsf, void *ptr, size_t size);
+extern void tlsf_free(tlsf_t tlsf, void *ptr);
 
 /* Returns internal block size, not original request size */
-size_t tlsf_block_size(void* ptr);
+extern size_t tlsf_block_size(void *ptr);
 
 /* Overheads/limits of internal structures. */
-size_t tlsf_size(void);
-size_t tlsf_align_size(void);
-size_t tlsf_block_size_min(void);
-size_t tlsf_block_size_max(void);
-size_t tlsf_pool_overhead(void);
-size_t tlsf_alloc_overhead(void);
+extern size_t tlsf_size(void);
+extern size_t tlsf_align_size(void);
+extern size_t tlsf_block_size_min(void);
+extern size_t tlsf_block_size_max(void);
+extern size_t tlsf_pool_overhead(void);
+extern size_t tlsf_alloc_overhead(void);
 
 /* Debugging. */
-typedef void (*tlsf_walker_t)(void* ptr, size_t size, int used, void* user);
+typedef void (*tlsf_walker_t)(void *ptr, size_t size, int used, void *user);
 
-void tlsf_walk_pool(pool_t pool, tlsf_walker_t walker, void* user);
+extern void tlsf_walk_pool(pool_t pool, tlsf_walker_t walker, void *user);
 /* Returns nonzero if any internal consistency check fails. */
-int tlsf_check(tlsf_t tlsf);
-int tlsf_check_pool(pool_t pool);
+extern int tlsf_check(tlsf_t tlsf);
+extern int tlsf_check_pool(pool_t pool);
 
-#if defined(__cplusplus)
-};
-#endif
+__END_DECLS
 
 #endif /* !_YAUL_KERNEL_MM_TLSF_H_ */
