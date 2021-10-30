f9aa0decd98b8a0c95fa655ff34693117f2e6d4c
Israel Jacquez <mrkotfw@gmail.com>
2021-10-30 11:32:00 -0700

    Add hack to silence output from pre/post-build-iso

--- libyaul/common/post.common.mk
+++ libyaul/common/post.common.mk
@@ -199,8 +199,10 @@ $(foreach SYMBOL_DIR,$(join $(ROMDISK_SYMBOLS),$(addprefix ;,$(SH_SRCS_ROMDISK))
 		$(basename $(call macro-word-split,$(SYMBOL_DIR),2)))))
 
 $(SH_PROGRAM).iso: $(SH_BUILD_PATH)/$(SH_PROGRAM).bin $(SH_BUILD_PATH)/IP.BIN
-	$(ECHO)$(MAKE) --no-print-directory -f $(THIS_FILE) pre-build-iso
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
+    # This is a rather nasty hack to suppress any output from running the
+    # pre/post-build-iso targets
+	$(ECHO)$(MAKE) --no-print-directory $$([ -z "$(SILENT)" ] || printf -- "-s") -f $(THIS_FILE) pre-build-iso
 	$(ECHO)mkdir -p $(IMAGE_DIRECTORY)
 	$(ECHO)cp $(SH_BUILD_PATH)/$(SH_PROGRAM).bin $(IMAGE_DIRECTORY)/$(IMAGE_1ST_READ_BIN)
 	$(ECHO)for txt in "ABS.TXT" "BIB.TXT" "CPY.TXT"; do \
@@ -209,7 +211,7 @@ $(SH_PROGRAM).iso: $(SH_BUILD_PATH)/$(SH_PROGRAM).bin $(SH_BUILD_PATH)/IP.BIN
 	    fi \
 	done
 	$(ECHO)$(YAUL_INSTALL_ROOT)/share/wrap-error $(YAUL_INSTALL_ROOT)/bin/make-iso $(IMAGE_DIRECTORY) $(SH_BUILD_PATH)/IP.BIN $(SH_PROGRAM)
-	$(ECHO)$(MAKE) --no-print-directory -f $(THIS_FILE) post-build-iso
+	$(ECHO)$(MAKE) --no-print-directory $$([ -z "$(SILENT)" ] || printf -- "-s") -f $(THIS_FILE) post-build-iso
 
 $(SH_PROGRAM).ss: $(SH_BUILD_PATH)/$(SH_PROGRAM).bin $(SH_BUILD_PATH)/CART-IP.BIN
 	@printf -- "$(V_BEGIN_YELLOW)$@$(V_END)\n"
