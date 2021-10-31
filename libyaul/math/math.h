ab72cf8428cdf9fe81703ad0f8ea2ec1d8ba8199
Israel Jacquez <mrkotfw@gmail.com>
2021-10-31 12:37:37 -0700

    Suppress GCC extension warnings

--- libyaul/math/math.h
+++ libyaul/math/math.h
@@ -23,7 +23,7 @@ __BEGIN_DECLS
 
 #ifndef min
 #define min(a, b)                                                              \
-        ({ __typeof__ (a) _a = (a);                                            \
+        __extension__ ({ __typeof__ (a) _a = (a);                              \
            __typeof__ (b) _b = (b);                                            \
            (_a < _b) ? _a : _b;                                                \
         })
@@ -31,7 +31,7 @@ __BEGIN_DECLS
 
 #ifndef max
 #define max(a, b)                                                              \
-        ({ __typeof__ (a) _a = (a);                                            \
+        __extension__ ({ __typeof__ (a) _a = (a);                              \
            __typeof__ (b) _b = (b);                                            \
            (_a > _b) ? _a : _b;                                                \
         })
@@ -39,14 +39,14 @@ __BEGIN_DECLS
 
 #ifndef sign
 #define sign(x)                                                                \
-        ({ __typeof__ (x) _x = (x);                                            \
+        __extension__ ({ __typeof__ (x) _x = (x);                              \
            (_x < 0) ? -1 : 1;                                                  \
         })
 #endif /* !sign */
 
 #ifndef clamp
 #define clamp(x, y, z)                                                         \
-        ({ __typeof__ (x) _x = (x);                                            \
+        __extension__ ({ __typeof__ (x) _x = (x);                              \
            __typeof__ (y) _y = (y);                                            \
            __typeof__ (z) _z = (z);                                            \
            (_x <= _y) ? _y : ((_x >= _z) ? _z : _x);                           \
