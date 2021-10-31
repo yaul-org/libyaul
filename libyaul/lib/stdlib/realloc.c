6d5e88021f388a2eeb05c67d3b400445accb60db
Israel Jacquez <mrkotfw@gmail.com>
2021-10-31 11:35:24 -0700

    Extern _internal_user_* within functions

--- libyaul/lib/stdlib/realloc.c
+++ libyaul/lib/stdlib/realloc.c
@@ -5,5 +5,7 @@
 void * __weak
 realloc(void *old, size_t new_len)
 {
+        extern void *_internal_user_realloc(void *old, size_t new_len);
+
         return _internal_user_realloc(old, new_len);
 }
