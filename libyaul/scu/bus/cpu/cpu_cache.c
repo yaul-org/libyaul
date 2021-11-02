aab83f2df94838f5b2ff7c7f8b59893125b186bc
Israel Jacquez <mrkotfw@gmail.com>
2021-11-02 09:03:08 -0700

    Add support for reading CPU cache for debugging

--- libyaul/scu/bus/cpu/cpu_cache.c
+++ libyaul/scu/bus/cpu/cpu_cache.c
@@ -54,3 +54,36 @@ cpu_cache_purge(void)
         t0 |= 0x01;
         *reg_ccr = t0; /* Enable cache */
 }
+
+void __section(".uncached")
+cpu_cache_data_way_read(uint8_t way, cpu_cache_data_way_t *data_way)
+{
+        volatile uint8_t * const reg_ccr = (volatile uint8_t * const)CPU(CCR);
+
+        uint8_t t0;
+        t0 = *reg_ccr;
+
+        /* Specify which way to read from */
+        t0 &= ~0xC0;
+        t0 |= (way & 3) << 6;
+
+        *reg_ccr = t0;
+
+        volatile uint32_t *cache_data_rw;
+        cache_data_rw = (volatile uint32_t *)CPU_CACHE_ADDRESS_RW;
+
+        /* Force uncache writes in case the user forgets */
+        volatile uint32_t *uncached_cache_way;
+        uncached_cache_way = (volatile uint32_t *)(CPU_CACHE_THROUGH | (uintptr_t)data_way->data);
+
+        const uint32_t count = CPU_CACHE_WAY_SIZE / CPU_CACHE_LINE_SIZE;
+
+        for (uint32_t i = 0; i < count; i++) {
+                *uncached_cache_way = *cache_data_rw;
+
+                uncached_cache_way++;
+                /* Skip the rest of the cache line, as it's the same data
+                 * repeated 4 times */
+                cache_data_rw += 4;
+        }
+}
