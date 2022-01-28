8c5a91a112199da30b371153220d76f3a6446141
Israel Jacquez <mrkotfw@gmail.com>
2022-01-28 13:16:31 -0800

    Improve MEMB implementation
    - Simplify memb_alloc()
    - Add memb_memb_*() for defining dynamic MEMBs
    - Add memb_contiguous_alloc() to allocate contiguous blocks

--- libyaul/kernel/mm/memb.c
+++ libyaul/kernel/mm/memb.c
@@ -1,6 +1,6 @@
 /*-
  * Copyright (c) 2004 Swedish Institute of Computer Science.
- * Copyright (c) 2012-2019 Israel Jacquez
+ * Copyright (c) 2012-2022 Israel Jacquez
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,6 +30,8 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
 #include "memb.h"
 
@@ -37,168 +39,237 @@
  * Returns 1 if PTR is within bounds of the block pool NAME.
  */
 #define MEMB_PTR_BOUND(name, ptr)                                              \
-        (((int8_t *)(ptr) >= (int8_t *)(name)->m_bpool) &&                     \
-         ((int8_t *)(ptr) < ((int8_t *)(name)->m_bpool +                       \
-             ((name)->m_bnum * (name)->m_bsize))))
+        (((int8_t *)(ptr) >= (int8_t *)(name)->pool) &&                        \
+         ((int8_t *)(ptr) < ((int8_t *)(name)->pool +                          \
+             ((name)->count * (name)->size))))
 
-static void _incr_block_index(memb_t *);
+static inline uint32_t _block_index_wrap(const memb_t *memb, uint32_t index) __always_inline;
 
 /*
- * Initialize a block pool MB.
+ * Initialize a block pool MEMB.
  */
 void
-memb_init(memb_t *mb)
+memb_init(memb_t *memb)
 {
-        uint32_t bidx;
-
-        for (bidx = 0; bidx < mb->m_bnum; bidx++) {
-                mb->m_breftype[bidx] = MEMB_REF_AVAILABLE;
+        for (uint32_t i = 0; i < memb->count; i++) {
+                memb->ref[i].count = 0;
         }
 
-        mb->m_size = 0;
+        memb->next_index = 0;
+        memb->alloc_count = 0;
 
-        (void)memset(mb->m_bpool, 0x00, mb->m_bsize * mb->m_bnum);
+        (void)memset(memb->pool, 0x00, memb->size * memb->count);
+}
+
+memb_t *
+memb_memb_alloc(uint32_t block_count, uint32_t block_size, uint32_t align)
+{
+        block_count = max(block_count, 4UL);
+        align = max(align, 4UL);
+
+        const size_t ref_size = sizeof(memb_ref_t) * block_count;
+        const size_t pool_size = block_size * block_count;
+
+        /* Allocate all memory needed in a single request */
+        void * const area = malloc(sizeof(memb_t) + ref_size + pool_size);
+
+        if (area == NULL) {
+                return NULL;
+        }
+
+        uintptr_t area_ptr;
+        area_ptr = (uintptr_t)area;
+
+        /* Have the memb_t structure be at the top of the allocation request.
+         * This way, we can easily free */
+        memb_t * const memb = (void *)area_ptr;
+        area_ptr += sizeof(memb_t);
+
+        memb->size = block_size;
+        memb->count = block_count;
+
+        memb->ref = (void *)area_ptr;
+        area_ptr += ref_size;
+
+        memb->pool = (void *)area_ptr;
+
+        memb_init(memb);
+
+        return memb;
+}
+
+void
+memb_memb_free(memb_t *memb)
+{
+        if (memb == NULL) {
+                return;
+        }
+
+        free(memb);
 }
 
 /*-
- * Allocate a unit block from the block pool MB.
+ * Allocate a unit block from the block pool MEMB.
  *
  * If successful, pointer to block is returned. Otherwise NULL is
  * returned for the following cases:
  *
- *   - MEMB MB is NULL; or
+ *   - MEMB is NULL; or
  *   - There are no free block(s) to allocate
  */
 void *
-memb_alloc(memb_t *mb)
+memb_alloc(memb_t *memb)
 {
-        uint32_t bidx;
-        uint32_t cur_bidx;
-        int8_t *block;
-
-        if (mb == NULL) {
+        if (memb == NULL) {
                 return NULL;
         }
 
         /* Are we full? */
-        if (mb->m_bidx >= mb->m_bnum) {
+        if (memb->alloc_count == memb->count) {
                 return NULL;
         }
 
-        /* Try next-fit method */
-        bidx = mb->m_bidx;
+        while (true) {
+                if (memb->ref[memb->next_index].count == 0) {
+                        break;
+                }
 
-        if (mb->m_breftype[bidx] == MEMB_REF_AVAILABLE) {
-                goto allocate;
+                memb->next_index = _block_index_wrap(memb, memb->next_index + 1);
         }
 
-        /* Try first-fit method */
-        cur_bidx = mb->m_bidx;
+        void * const block = (void *)((uintptr_t)memb->pool +
+            (memb->next_index * memb->size));
 
-        do {
-                _incr_block_index(mb);
-                bidx = mb->m_bidx;
+        memb->ref[memb->next_index].count = 1;
+        memb->next_index = _block_index_wrap(memb, memb->next_index + 1);
+        memb->alloc_count++;
 
-                if (mb->m_breftype[bidx] == MEMB_REF_AVAILABLE) {
-                        goto allocate;
+        return block;
+}
+
+void *
+memb_contiguous_alloc(memb_t *memb, uint32_t count)
+{
+        if (memb == NULL) {
+                return NULL;
+        }
+
+        if (count == 0) {
+                return NULL;
+        }
+
+        if (count == 1) {
+                return memb_alloc(memb);
+        }
+
+        /* Are we full? */
+        if (memb->alloc_count == memb->count) {
+                return NULL;
+        }
+
+        uint32_t prev_index;
+        prev_index = 0;
+        uint32_t next_index;
+        next_index = memb->next_index;
+
+        for (uint32_t contiguous_count = 0; ; ) {
+                if (memb->ref[next_index].count == 0) {
+                        contiguous_count++;
                 }
-        } while (cur_bidx != bidx);
 
-        /* No free block was found, so we return NULL to indicate
-         * failure to allocate block. */
-        return NULL;
+                if (contiguous_count == count) {
+                        break;
+                }
 
-allocate:
-        block = (int8_t *)mb->m_bpool + (bidx * mb->m_bsize);
-        mb->m_breftype[bidx] = MEMB_REF_RESERVED;
-        /* Increase to next block index */
-        _incr_block_index(mb);
+                prev_index = next_index;
+                next_index = _block_index_wrap(memb, next_index + 1);
 
-        mb->m_size++;
+                /* Case for when the next index is wrapped to the beginning
+                 * array */
+                if ((contiguous_count > 0) && (next_index < prev_index)) {
+                        return NULL;
+                }
+        }
 
-        return (void *)block;
+        const uint32_t start_index = next_index - (count - 1);
+
+        for (uint32_t i = start_index; i <= next_index; i++) {
+                memb->ref[i].count = count;
+        }
+
+        memb->next_index = _block_index_wrap(memb, next_index + 1);
+        memb->alloc_count += count;
+
+        void * const block = (void *)((uintptr_t)memb->pool +
+            (start_index * memb->size));
+
+        return block;
 }
 
 /*
  * Free the unit block as dirty.
  *
- * If successful, 0 is returned. Otherwise -1 is returned if the
- * address is not within the bounds of the block pool MB.
+ * If successful, 0 is returned. Otherwise -1 is returned if the address is not
+ * within the bounds of the block pool MEMB.
  */
 int
-memb_free(memb_t *mb, void *addr)
+memb_free(memb_t *memb, void *addr)
 {
-        if (mb == NULL) {
+        if (memb == NULL) {
                 return -1;
         }
 
-        /* Not within bounds. */
-        if (!MEMB_PTR_BOUND(mb, addr)) {
+        /* Not within bounds */
+        if (!MEMB_PTR_BOUND(memb, addr)) {
                 return -1;
         }
 
-        int8_t *block;
-        block = (int8_t *)mb->m_bpool;
+        const uintptr_t pool_ptr = (uintptr_t)memb->pool;
+        const uintptr_t addr_ptr = (uintptr_t)addr;
 
-        /* Walk through the list of blocks and try to find the block to
-         * which the pointer address points to. */
-        uint32_t bidx;
+        const uint32_t block_index = (addr_ptr - pool_ptr) / memb->size;
+        memb_ref_t * const ref = &memb->ref[block_index];
 
-        for (bidx = 0; bidx < mb->m_bnum; bidx++) {
-                if (block == (int8_t *)addr) {
-                        if (mb->m_breftype[bidx] == MEMB_REF_AVAILABLE) {
-                                break;
-                        }
+        const uint32_t contiguous_count = ref[0].count;
 
-                        mb->m_breftype[bidx] = MEMB_REF_AVAILABLE;
-                        mb->m_size--;
-
-                        /* Set recently freed block to next block
-                         * index */
-                        mb->m_bidx = (mb->m_size == 0) ? 0 : bidx;
-
-                        return 0;
-                }
-
-                block += mb->m_bsize;
+        for (uint32_t i = 0; i < contiguous_count; i++) {
+                ref[i].count = 0;
         }
 
-        return -1;
+        memb->next_index = block_index;
+        memb->alloc_count -= contiguous_count;
+
+        return 0;
 }
 
 /*
  * Return the number of blocks allocated.
  *
- * If successful, the number of blocks allocated is returned. Otherwise
- * -1 is returned.
+ * If successful, the number of blocks allocated is returned. Otherwise -1 is
+ * returned.
  */
 int32_t
-memb_size(memb_t *mb)
+memb_size(memb_t *memb)
 {
-        if (mb == NULL) {
+        if (memb == NULL) {
                 return -1;
         }
 
-        return mb->m_size;
+        return memb->alloc_count;
 }
 
 /*
- * Determine if ADDR is within bounds of the block pool MB. Otherwise 0
- * is returned.
- *
+ * Determine if ADDR is within bounds of the block pool MEMB. Otherwise 0 is
+ * returned.
  */
 bool
-memb_bounds(memb_t *mb, void *addr)
+memb_bounds(memb_t *memb, void *addr)
 {
-        return MEMB_PTR_BOUND(mb, addr);
+        return MEMB_PTR_BOUND(memb, addr);
 }
 
-static inline void __always_inline
-_incr_block_index(memb_t *mb)
+static inline uint32_t __always_inline
+_block_index_wrap(const memb_t *memb, uint32_t index)
 {
-        if (mb->m_bidx >= (mb->m_bnum - 1)) {
-                mb->m_bidx = 0;
-        }
-
-        mb->m_bidx++;
+        return ((index >= memb->count) ? 0 : index);
 }
