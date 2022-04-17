4ed916f1e448aea38986910e776dc9d25781ab4d
Israel Jacquez <mrkotfw@gmail.com>
2022-04-17 14:36:46 -0700

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,2 @@
-;;;           ;; All (major?) modes
-;;; ((nil   . ((x1 . y1)
-;;;            (x2 . y2)))
-;;;  (mode1 . ((x1 . y1)
-;;;            (x2 . y2)))
-;;;  (mode2 . ((x1 . y1))))
-
 ((nil . ((magit-todos-depth . 1)
          (eval . (add-to-list 'auto-mode-alist '("\\.sx\\'" . asm-mode))))))
