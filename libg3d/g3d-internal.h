8caf14aaa1039b1b74d10f74c282d9ed02d63a33
Israel Jacquez <mrkotfw@gmail.com>
2022-04-21 02:29:53 -0700

    Fix support for multiple objects

--- libg3d/g3d-internal.h
+++ libg3d/g3d-internal.h
@@ -63,7 +63,7 @@ typedef struct {
         FIXED z_value;                 /* Z value of the current polygon */
         const transform_proj_t *polygon[4]; /* Pointers to the pool that make up the current polygon */
 
-        const g3d_object_t *object; /* Current object */
+        const g3d_object_t *object;    /* Current object */
         const void *xpdata;            /* Current XPDATA */
 
         vdp1_cmdt_orderlist_t *current_orderlist;
